f7dcb7fc3ecdb35711e5a7488599ee4b0a501053
Yue Zhang <yuezha@microsoft.com>
2014-09-24 10:10:09 +0800

    Add protocol detection basic logic

--- azure/linuxagent/protocol.py
+++ azure/linuxagent/protocol.py
@@ -19,21 +19,65 @@
 # Requires Python 2.4+ and Openssl 1.0+
 #
 
-ProtocolV1File = os.path.join(LibDir, 'protocolv1')
-ProtocolV2File = os.path.join(LibDir, 'protocolv2')
+from azure.linuxagent.util import *
+from azure.linuxagent.logger import *
+
+__ProtocolV1FilePath = os.path.join(LibDir, 'protocolv1')
+__ProtocolV2FilePath = os.path.join(LibDir, 'protocolv2')
+
+__SleepDurations = [0, 10, 30, 60, 60]
 
 def DetectEndpoint():
+    detected = False
+    for duration in __SleepDurations:
+        Log("Detect endpoint...")
+        OpenPortForDhcp()
+        if(_DetectEndpoint()):
+            detected = True
+            break
+        sleep(duration)
+        RestartNetwork()
+
+    if not detected:
+        raise Exception("Detect endpoint failed.") 
+
+def _DetectEndpoint():
+    metadataServer = DetectMetadataServer()
+    if metadataServer:
+        SetFileContent(__ProtocolV2FilePath, '')
+        return True
+    else:
+        os.remove(__ProtocolV2FilePath)
+
+    wireServer = DetectWireServer()
+    if wireServer:
+        SetFileContent(__ProtocolV1FilePath, wireServer)
+        return True
+    else:
+        os.remove(__ProtocolV1FilePath)
+
+    return False
+
+__MeatadataServerAddr=''
+def DetectMetadataServer():
+    pass
+
+def DetectWireServer():
     pass
 
 def GetProtocol():
-    if os.path.isfile(ProtocolV2File):
+    if os.path.isfile(__ProtocolV2FilePath):
         return ProtocolV2()
-    elif os.path.isfile(ProtocolV1File):
-        return ProtocolV1()
+    elif os.path.isfile(__ProtocolV1FilePath):
+        wireServer = GetFileContent(__ProtocolV1FilePath)
+        return ProtocolV1(wireServer)
     else:
         raise Exeption("Endpoint not detected")
 
-class Protocol():
+class ProtocolV1(Protocol):
+
+    def __init__(self, endpoint):
+        self.endpoint = endpoint
 
     def getVmInfo(self):
         pass
@@ -59,8 +103,29 @@ class Protocol():
     def reportEvent(self):
         pass
 
-class ProtocolV1(Protocol):
-    pass
-
 class ProtocolV2(protocol):
-    pass
+
+    def getVmInfo(self):
+        pass
+
+    def getCerts(self):
+        pass
+
+    def getExtensions(self):
+        pass
+
+    def getOvf(self):
+        pass
+
+    def reportProvisionStatus(self):
+        pass
+
+    def reportAgentStatus(self):
+        pass
+
+    def reportExtensionStatus(self):
+        pass
+
+    def reportEvent(self):
+        pass
+
