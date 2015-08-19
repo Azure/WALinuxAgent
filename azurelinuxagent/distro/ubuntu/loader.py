0ce339b6dfd30eb37b6be07c4cd72417e0ba8ebf
lizzha <zhangli8810@gmail.com>
2015-08-19 14:48:06 +0800

    Update folder structure, and resolve comments

--- azurelinuxagent/distro/ubuntu/loader.py
+++ azurelinuxagent/distro/ubuntu/loader.py
@@ -17,29 +17,19 @@
 # Requires Python 2.4+ and Openssl 1.0+
 #
 
-from azurelinuxagent.metadata import DISTRO_NAME, DISTRO_VERSION
+from azurelinuxagent.metadata import DISTRO_NAME, DISTRO_VERSION, DISTRO_FULL_NAME
 
 def get_osutil():
     from  azurelinuxagent.distro.ubuntu.osutil import Ubuntu1204OSUtil, \
                                                       UbuntuOSUtil, \
                                                       Ubuntu14xOSUtil, \
                                                       UbuntuSnappyOSUtil
-    import os
-    import azurelinuxagent.utils.fileutil as fileutil
-
-    # TODO: https://bugs.launchpad.net/snappy/+bug/1481086
-    def is_snappy():
-        if os.path.exists("/etc/motd"):
-            motd = fileutil.read_file("/etc/motd")
-            if "snappy" in motd:
-                return True
-        return False
 
     if DISTRO_VERSION == "12.04":
         return Ubuntu1204OSUtil()
     elif DISTRO_VERSION == "14.04" or DISTRO_VERSION == "14.10":
         return Ubuntu14xOSUtil()
-    elif is_snappy():
+    elif DISTRO_FULL_NAME == "Snappy Ubuntu Core":
         return UbuntuSnappyOSUtil()
     else:
         return UbuntuOSUtil()
