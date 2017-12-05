14d960f56da47062527959c53ed6a4a1c6c885a2
Bastian Blank <bastian.blank@credativ.de>
2017-12-05 09:16:05 +0100

    Don't install sysv files on Debian

--- setup.py
+++ setup.py
@@ -158,7 +158,6 @@ def get_data_files(name, version, fullname):
         set_conf_files(data_files, src=["config/debian/waagent.conf"])
         set_logrotate_files(data_files)
         set_udev_files(data_files, dest="/lib/udev/rules.d")
-        set_sysv_files(data_files)
     else:
         # Use default setting
         set_bin_files(data_files)
