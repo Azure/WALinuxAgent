748ce83d38c82b784fb1a425a0456c88d16119c1
William Pearson <William.Pearson@microsoft.com>
2018-03-02 17:16:28 -0800

    Fix bug regarding downgrade
    Instead of raising an exception, log and report an error if updating an
    exception would be a downgrade.

--- azurelinuxagent/ga/exthandlers.py
+++ azurelinuxagent/ga/exthandlers.py
@@ -390,7 +390,12 @@ class ExtHandlersHandler(object):
         old_ext_handler_i = ext_handler_i.get_installed_ext_handler()
         if old_ext_handler_i is not None and \
            old_ext_handler_i.version_gt(ext_handler_i):
-            raise ExtensionError(u"Downgrade not allowed")
+            msg = "Downgrade is not allowed. Skipping install and enable."
+            ext_handler_i.logger.error(msg)
+            ext_handler_i.set_operation(WALAEventOperation.Downgrade)
+            ext_handler_i.report_event(message=ustr(msg), is_success=True)
+            return
+
         handler_state = ext_handler_i.get_handler_state()
         ext_handler_i.logger.info("[Enable] current handler state is: {0}",
                                   handler_state.lower())
