f6e9fdc4c1ac385db1fc758857f74a444216e072
Christopher Boumenot <chrboum@microsoft.com>
2018-04-16 23:14:57 -0700

    add imds metadata to telemetry

---
+++ azurelinuxagent/common/protocol/imds.py
@@ -0,0 +1,89 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+import json
+
+import azurelinuxagent.common.utils.restutil as restutil
+from azurelinuxagent.common.exception import HttpError, ProtocolError
+from azurelinuxagent.common.future import ustr
+from azurelinuxagent.common.protocol.restapi import DataContract, set_properties
+
+IMDS_ENDPOINT = '169.254.169.254'
+APIVERSION = '2017-08-01'
+BASE_URI = "http://{0}/metadata/instance/{1}?api-version={2}"
+
+
+def get_imds_client():
+    return ImdsClient()
+
+
+class ComputeInfo(DataContract):
+    def __init__(self,
+                 location=None,
+                 name=None,
+                 offer=None,
+                 osType=None,
+                 placementGroupId=None,
+                 platformFaultDomain=None,
+                 placementUpdateDomain=None,
+                 publisher=None,
+                 resourceGroupName=None,
+                 sku=None,
+                 subscriptionId=None,
+                 tags=None,
+                 version=None,
+                 vmId=None,
+                 vmSize=None):
+        self.location = location
+        self.name = name
+        self.offer = offer
+        self.osType = osType
+        self.placementGroupId = placementGroupId
+        self.platformFaultDomain = platformFaultDomain
+        self.platformUpdateDomain = placementUpdateDomain
+        self.publisher = publisher
+        self.resourceGroupName = resourceGroupName
+        self.sku = sku
+        self.subscriptionId = subscriptionId
+        self.tags = tags
+        self.version = version
+        self.vmId = vmId
+        self.vmSize = vmSize
+
+    @property
+    def image_info(self):
+        return "{0}:{1}:{2}:{3}".format(self.publisher, self.offer, self.sku, self.version)
+
+
+class ImdsClient(object):
+    def __init__(self, version=APIVERSION):
+        self._api_version = version
+        self._headers = {
+            'User-Agent': restutil.HTTP_USER_AGENT,
+            'Metadata': True,
+        }
+        pass
+
+    @property
+    def compute_url(self):
+        return BASE_URI.format(IMDS_ENDPOINT, 'compute', self._api_version)
+
+    def get_compute(self):
+        """
+        Fetch compute information.
+
+        :return: instance of a ComputeInfo
+        :rtype: ComputeInfo
+        """
+
+        resp = restutil.http_get(self.compute_url, headers=self._headers)
+
+        if restutil.request_failed(resp):
+            raise HttpError("{0} - GET: {1}".format(resp.status, self.compute_url))
+
+        data = resp.read()
+        data = json.loads(ustr(data, encoding="utf-8"))
+
+        compute_info = ComputeInfo()
+        set_properties('compute', compute_info, data)
+
+        return compute_info
---
+++ tests/protocol/test_imds.py
@@ -0,0 +1,100 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+import json
+
+from azurelinuxagent.common.exception import HttpError
+from azurelinuxagent.common.protocol.imds import ComputeInfo, ImdsClient
+from azurelinuxagent.common.protocol.restapi import set_properties
+from azurelinuxagent.common.utils import restutil
+from tests.ga.test_update import ResponseMock
+from tests.tools import *
+
+
+class TestImds(AgentTestCase):
+    @patch("azurelinuxagent.ga.update.restutil.http_get")
+    def test_get(self, mock_http_get):
+        mock_http_get.return_value = ResponseMock(response='''{
+        "location": "westcentralus",
+        "name": "unit_test",
+        "offer": "UnitOffer",
+        "osType": "Linux",
+        "placementGroupId": "",
+        "platformFaultDomain": "0",
+        "platformUpdateDomain": "0",
+        "publisher": "UnitPublisher",
+        "resourceGroupName": "UnitResourceGroupName",
+        "sku": "UnitSku",
+        "subscriptionId": "e4402c6c-2804-4a0a-9dee-d61918fc4d28",
+        "tags": "Key1:Value1;Key2:Value2",
+        "vmId": "f62f23fb-69e2-4df0-a20b-cb5c201a3e7a",
+        "version": "UnitVersion",
+        "vmSize": "Standard_D1_v2"
+        }'''.encode('utf-8'))
+
+        test_subject = ImdsClient()
+        test_subject.get_compute()
+
+        self.assertEqual(1, mock_http_get.call_count)
+        positional_args, kw_args = mock_http_get.call_args
+
+        self.assertEqual('http://169.254.169.254/metadata/instance/compute?api-version=2017-08-01', positional_args[0])
+        self.assertTrue('User-Agent' in kw_args['headers'])
+        self.assertTrue('Metadata' in kw_args['headers'])
+        self.assertEqual(True, kw_args['headers']['Metadata'])
+
+    @patch("azurelinuxagent.ga.update.restutil.http_get")
+    def test_get_bad_request(self, mock_http_get):
+        mock_http_get.return_value = ResponseMock(status=restutil.httpclient.BAD_REQUEST)
+
+        test_subject = ImdsClient()
+        self.assertRaises(HttpError, test_subject.get_compute)
+
+    @patch("azurelinuxagent.ga.update.restutil.http_get")
+    def test_get_empty_response(self, mock_http_get):
+        mock_http_get.return_value = ResponseMock(response=''.encode('utf-8'))
+
+        test_subject = ImdsClient()
+        self.assertRaises(ValueError, test_subject.get_compute)
+
+    def test_deserialize_ComputeInfo(self):
+        s = '''{
+        "location": "westcentralus",
+        "name": "unit_test",
+        "offer": "UnitOffer",
+        "osType": "Linux",
+        "placementGroupId": "",
+        "platformFaultDomain": "0",
+        "platformUpdateDomain": "0",
+        "publisher": "UnitPublisher",
+        "resourceGroupName": "UnitResourceGroupName",
+        "sku": "UnitSku",
+        "subscriptionId": "e4402c6c-2804-4a0a-9dee-d61918fc4d28",
+        "tags": "Key1:Value1;Key2:Value2",
+        "vmId": "f62f23fb-69e2-4df0-a20b-cb5c201a3e7a",
+        "version": "UnitVersion",
+        "vmSize": "Standard_D1_v2"
+        }'''
+
+        data = json.loads(s, encoding='utf-8')
+
+        compute_info = ComputeInfo()
+        set_properties("compute", compute_info, data)
+
+        self.assertEqual('westcentralus', compute_info.location)
+        self.assertEqual('unit_test', compute_info.name)
+        self.assertEqual('UnitOffer', compute_info.offer)
+        self.assertEqual('Linux', compute_info.osType)
+        self.assertEqual('', compute_info.placementGroupId)
+        self.assertEqual('0', compute_info.platformFaultDomain)
+        self.assertEqual('0', compute_info.platformUpdateDomain)
+        self.assertEqual('UnitPublisher', compute_info.publisher)
+        self.assertEqual('UnitResourceGroupName', compute_info.resourceGroupName)
+        self.assertEqual('UnitSku', compute_info.sku)
+        self.assertEqual('e4402c6c-2804-4a0a-9dee-d61918fc4d28', compute_info.subscriptionId)
+        self.assertEqual('Key1:Value1;Key2:Value2', compute_info.tags)
+        self.assertEqual('f62f23fb-69e2-4df0-a20b-cb5c201a3e7a', compute_info.vmId)
+        self.assertEqual('UnitVersion', compute_info.version)
+        self.assertEqual('Standard_D1_v2', compute_info.vmSize)
+
+        self.assertEqual('UnitPublisher:UnitOffer:UnitSku:UnitVersion', compute_info.image_info)
+
