1c5ca3cc1f9a373760913e37d4e33f27c8b8116c
Matt Atkins <matkin@microsoft.com>
2018-05-31 11:22:34 -0700

    Adding RemoteAccess.xml parsing tests and data

--- tests/ga/test_remoteaccess.py
+++ tests/ga/test_remoteaccess.py
@@ -41,6 +41,43 @@ class TestRemoteAccess(AgentTestCase):
             return
         self.fail("expected exception did not occur.")
 
+    def test_parse_two_remote_access_accounts(self):
+        data_str = load_data('wire/remote_access_two_accounts.xml')
+        remote_access = RemoteAccess(data_str)
+        self.assertNotEquals(None, remote_access)
+        self.assertEquals("1", remote_access.incarnation)
+        self.assertEquals(2, len(remote_access.user_list.users), "User count does not match.")
+        self.assertEquals("testAccount1", remote_access.user_list.users[0].name, "Account name does not match")
+        self.assertEquals("encryptedPasswordString", remote_access.user_list.users[0].encrypted_password, "Encrypted password does not match.")
+        self.assertEquals("2019-01-01", remote_access.user_list.users[0].expiration, "Expiration does not match.")
+        self.assertEquals("testAccount2", remote_access.user_list.users[1].name, "Account name does not match")
+        self.assertEquals("encryptedPasswordString", remote_access.user_list.users[1].encrypted_password, "Encrypted password does not match.")
+        self.assertEquals("2019-01-01", remote_access.user_list.users[1].expiration, "Expiration does not match.")
+
+    def test_parse_ten_remote_access_accounts(self):
+        data_str = load_data('wire/remote_access_10_accounts.xml')
+        remote_access = RemoteAccess(data_str)
+        self.assertNotEquals(None, remote_access)
+        self.assertEquals(10, len(remote_access.user_list.users), "User count does not match.")
+    
+    def test_parse_duplicate_remote_access_accounts(self):
+        data_str = load_data('wire/remote_access_duplicate_accounts.xml')
+        remote_access = RemoteAccess(data_str)
+        self.assertNotEquals(None, remote_access)
+        self.assertEquals(2, len(remote_access.user_list.users), "User count does not match.")
+        self.assertEquals("testAccount", remote_access.user_list.users[0].name, "Account name does not match")
+        self.assertEquals("encryptedPasswordString", remote_access.user_list.users[0].encrypted_password, "Encrypted password does not match.")
+        self.assertEquals("2019-01-01", remote_access.user_list.users[0].expiration, "Expiration does not match.")
+        self.assertEquals("testAccount", remote_access.user_list.users[1].name, "Account name does not match")
+        self.assertEquals("encryptedPasswordString", remote_access.user_list.users[1].encrypted_password, "Encrypted password does not match.")
+        self.assertEquals("2019-01-01", remote_access.user_list.users[1].expiration, "Expiration does not match.")
+
+    def test_parse_zero_remote_access_accounts(self):
+        data_str = load_data('wire/remote_access_no_accounts.xml')
+        remote_access = RemoteAccess(data_str)
+        self.assertNotEquals(None, remote_access)
+        self.assertEquals(0, len(remote_access.user_list.users), "User count does not match.")
+
     @patch('azurelinuxagent.common.protocol.wire.WireClient.get_goal_state',
     return_value=GoalState(load_data('wire/goal_state_remote_access.xml')))
     @patch('azurelinuxagent.common.protocol.wire.WireClient.fetch_config',
@@ -54,8 +91,3 @@ class TestRemoteAccess(AgentTestCase):
         self.assertEquals(1, len(protocol.client.remote_access.user_list.users))
         self.assertEquals('testAccount', protocol.client.remote_access.user_list.users[0].name)
         self.assertEquals('encryptedPasswordString', protocol.client.remote_access.user_list.users[0].encrypted_password)
-
-
-
-
-
