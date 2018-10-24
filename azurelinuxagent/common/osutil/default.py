95733feb9e7ce6d36d735205b1900e1ec20f797b
Riccardo Pittau <elfosardo@gmail.com>
2018-10-24 12:23:17 +0200

    converting candidates to list during generation instead of multiple later conversion

--- azurelinuxagent/common/osutil/default.py
+++ azurelinuxagent/common/osutil/default.py
@@ -912,12 +912,11 @@ class DefaultOSUtil(object):
         def is_default(route):
             return route.destination == DEFAULT_DEST and int(route.flags) & RTF_GATEWAY == RTF_GATEWAY
 
-        candidates = filter(is_default, DefaultOSUtil.get_list_of_routes(route_table))
+        candidates = list(filter(is_default, DefaultOSUtil.get_list_of_routes(route_table)))
 
-        if len(list(candidates)) > 0:
+        if len(candidates) > 0:
             def get_metric(route):
                 return int(route.metric)
-
             primary_route = min(candidates, key=lambda candidate: candidate.metric)
             primary = primary_route.interface
             primary_metric = get_metric(primary_route)
