d6b4de0d30167653aed93f6b13f989f398086901
nam <narrieta@microsoft.com>
2018-12-09 13:31:20 -0800

    Revert "Remove __main__ module" (commit a85e6cd2d21450bf98bcee88e52d396aec58035f).

--- setup.py
+++ setup.py
@@ -231,6 +231,7 @@ setuptools.setup(
     url='https://github.com/Azure/WALinuxAgent',
     license='Apache License Version 2.0',
     packages=find_packages(exclude=["tests*"]),
+    py_modules=["__main__"],
     install_requires=requires,
     cmdclass={
         'install': install
