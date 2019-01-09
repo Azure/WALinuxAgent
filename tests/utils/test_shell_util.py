f3df4f33fb95583e8e1c34921f0ed82a15d18000
Norberto Arrieta <narrieta@users.noreply.github.com>
2019-01-09 09:49:24 -0800

    run_get_output: log expected errors as info (#1437)
    * run_get_output: log expected errors as info
    
    * add extra checks
    
    * python 3.4 compat
    
    * python 3.4 compat

--- tests/utils/test_shell_util.py
+++ tests/utils/test_shell_util.py
@@ -40,5 +40,60 @@ class TestrunCmd(AgentTestCase):
         self.assertEqual("\'foo bar\'", shellutil.quote("foo bar"))
         self.assertEqual("'foo'\\''bar'", shellutil.quote("foo\'bar"))
 
+    def test_it_should_log_command_failures_as_errors(self):
+        return_code = 99
+        command = "exit {0}".format(return_code)
+
+        with patch("azurelinuxagent.common.utils.shellutil.logger", autospec=True) as mock_logger:
+            shellutil.run_get_output(command, log_cmd=False)
+
+        self.assertEquals(mock_logger.error.call_count, 1)
+
+        args, kwargs = mock_logger.error.call_args
+        message = args[0]  # message is similar to "Command: [exit 99], return code: [99], result: []"
+        self.assertIn("[{0}]".format(command), message)
+        self.assertIn("[{0}]".format(return_code), message)
+
+        self.assertEquals(mock_logger.verbose.call_count, 0)
+        self.assertEquals(mock_logger.info.call_count, 0)
+        self.assertEquals(mock_logger.warn.call_count, 0)
+
+    def test_it_should_log_expected_errors_as_info(self):
+        return_code = 99
+        command = "exit {0}".format(return_code)
+
+        with patch("azurelinuxagent.common.utils.shellutil.logger", autospec=True) as mock_logger:
+            shellutil.run_get_output(command, log_cmd=False, expected_errors=[return_code])
+
+        self.assertEquals(mock_logger.info.call_count, 1)
+
+        args, kwargs = mock_logger.info.call_args
+        message = args[0]  # message is similar to "Command: [exit 99], return code: [99], result: []"
+        self.assertIn("[{0}]".format(command), message)
+        self.assertIn("[{0}]".format(return_code), message)
+
+        self.assertEquals(mock_logger.verbose.call_count, 0)
+        self.assertEquals(mock_logger.warn.call_count, 0)
+        self.assertEquals(mock_logger.error.call_count, 0)
+
+    def test_it_should_log_unexpected_errors_as_errors(self):
+        return_code = 99
+        command = "exit {0}".format(return_code)
+
+        with patch("azurelinuxagent.common.utils.shellutil.logger", autospec=True) as mock_logger:
+            shellutil.run_get_output(command, log_cmd=False, expected_errors=[return_code + 1])
+
+        self.assertEquals(mock_logger.error.call_count, 1)
+
+        args, kwargs = mock_logger.error.call_args
+        message = args[0]  # message is similar to "Command: [exit 99], return code: [99], result: []"
+        self.assertIn("[{0}]".format(command), message)
+        self.assertIn("[{0}]".format(return_code), message)
+
+        self.assertEquals(mock_logger.info.call_count, 0)
+        self.assertEquals(mock_logger.verbose.call_count, 0)
+        self.assertEquals(mock_logger.warn.call_count, 0)
+
+
 if __name__ == '__main__':
     unittest.main()
