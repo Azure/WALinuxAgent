8fe7001250072b5f121514b3c9cf50fa39adc766
Ahmed Osman <ahmedosman@Ahmeds-MBP.guest.corp.microsoft.com>
2019-05-20 14:16:26 -0700

    fixed filepath bug, issue #1457

--- azurelinuxagent/common/osutil/redhat.py
+++ azurelinuxagent/common/osutil/redhat.py
@@ -87,6 +87,8 @@ class Redhat6xOSUtil(DefaultOSUtil):
     def set_dhcp_hostname(self, hostname):
         ifname = self.get_if_name()
         filepath = "/etc/sysconfig/network-scripts/ifcfg-{0}".format(ifname)
+        if not os.path.isfile(filepath):
+            os.makedirs(filepath)
         fileutil.update_conf_file(filepath,
                                   'DHCP_HOSTNAME',
                                   'DHCP_HOSTNAME={0}'.format(hostname))
