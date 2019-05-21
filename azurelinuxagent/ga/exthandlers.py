8d02446af0168b4c5a1df03091f79008d6103c20
Norberto Arrieta <narrieta@users.noreply.github.com>
2019-05-21 12:00:15 -0700

    API for extension cgroups (#1533)

--- azurelinuxagent/ga/exthandlers.py
+++ azurelinuxagent/ga/exthandlers.py
@@ -38,7 +38,7 @@ import azurelinuxagent.common.conf as conf
 import azurelinuxagent.common.logger as logger
 import azurelinuxagent.common.utils.fileutil as fileutil
 import azurelinuxagent.common.version as version
-from azurelinuxagent.common.cgroupconfigurator import CGroupConfigurator
+from azurelinuxagent.common.cgroupconfigurator import CGroupConfigurator, CGroupConfigurator_tmp
 from azurelinuxagent.common.cgroupstelemetry import CGroupsTelemetry
 from azurelinuxagent.common.errorstate import ErrorState, ERROR_STATE_DELTA_INSTALL
 from azurelinuxagent.common.event import add_event, WALAEventOperation, elapsed_milliseconds, report_event
@@ -313,7 +313,7 @@ class ExtHandlersHandler(object):
         # Finally, remove the directories and packages of the
         # uninstalled handlers
         for handler in handlers:
-            handler.rm_ext_handler_dir()
+            handler.remove_ext_handler()
             pkg = os.path.join(conf.get_lib_dir(), handler.get_full_name() + HANDLER_PKG_EXT)
             if os.path.isfile(pkg):
                 try:
@@ -467,7 +467,7 @@ class ExtHandlersHandler(object):
                 else:
                     old_ext_handler_i.update(version=ext_handler_i.ext_handler.properties.version)
                 old_ext_handler_i.uninstall()
-                old_ext_handler_i.rm_ext_handler_dir()
+                old_ext_handler_i.remove_ext_handler()
                 ext_handler_i.update_with_install()
         else:
             ext_handler_i.update_settings()
@@ -491,7 +491,7 @@ class ExtHandlersHandler(object):
             if handler_state == ExtHandlerState.Enabled:
                 ext_handler_i.disable()
             ext_handler_i.uninstall()
-        ext_handler_i.rm_ext_handler_dir()
+        ext_handler_i.remove_ext_handler()
 
     def report_ext_handlers_status(self):
         """
@@ -608,6 +608,7 @@ class ExtHandlerInstance(object):
         self.pkg = None
         self.pkg_file = None
         self.is_upgrade = False
+        self.cgroups = []
         self.logger = None
         self.set_logger()
 
@@ -822,7 +823,7 @@ class ExtHandlerInstance(object):
         self.report_event(message="Download succeeded", duration=duration)
 
     def initialize(self):
-        self.logger.info("Initialize extension directory")
+        self.logger.info("Initializing extension {0}".format(self.get_full_name()))
 
         # Add user execute permission to all files under the base dir
         for file in fileutil.get_all_files(self.get_base_dir()):
@@ -866,7 +867,10 @@ class ExtHandlerInstance(object):
 
         except IOError as e:
             fileutil.clean_ioerror(e, paths=[self.get_base_dir(), self.pkg_file])
-            raise ExtensionDownloadError(u"Failed to create status or config dir", e)
+            raise ExtensionDownloadError(u"Failed to initialize extension '{0}'".format(self.get_full_name()), e)
+
+        # Create cgroups for the extension
+        self.cgroups = CGroupConfigurator_tmp.get_instance().create_extension_cgroups(self.get_full_name())
 
         # Save HandlerEnvironment.json
         self.create_handler_env()
@@ -910,7 +914,7 @@ class ExtHandlerInstance(object):
         except ExtensionError as e:
             self.report_event(message=ustr(e), is_success=False)
 
-    def rm_ext_handler_dir(self):
+    def remove_ext_handler(self):
         try:
             zip_filename = "__".join(os.path.basename(self.get_base_dir()).split("-")) + ".zip"
             destination = os.path.join(conf.get_lib_dir(), zip_filename)
@@ -934,6 +938,9 @@ class ExtHandlerInstance(object):
             message = "Failed to remove extension handler directory: {0}".format(e)
             self.report_event(message=message, is_success=False)
             self.logger.warn(message)
+
+        # Also remove the cgroups for the extension
+        CGroupConfigurator_tmp.get_instance().remove_extension_cgroups(self.get_full_name())
 
     def update(self, version=None):
         if version is None:
@@ -1130,30 +1137,16 @@ class ExtHandlerInstance(object):
                     # as root-relative. (Issue #1170)
                     full_path = os.path.join(base_dir, cmd.lstrip(os.path.sep))
 
-                    def pre_exec_function():
-                        """
-                        Change process state before the actual target process is started. Effectively, this runs between
-                        the fork() and the exec() of sub-process creation.
-                        """
-                        os.setsid()
-                        CGroupConfigurator.add_to_extension_cgroup(self.ext_handler.name, os.getpid())
+                    process = CGroupConfigurator_tmp.get_instance().start_extension_command(
+                        extension_name=self.get_full_name(),
+                        command=full_path,
+                        cwd=base_dir,
+                        env=env,
+                        stdout=stdout,
+                        stderr=stderr)
 
-                    process = subprocess.Popen(full_path,
-                                               shell=True,
-                                               cwd=base_dir,
-                                               stdout=stdout,
-                                               stderr=stderr,
-                                               env=env,
-                                               preexec_fn=pre_exec_function)
                 except OSError as e:
-                    raise ExtensionOperationError("Failed to launch '{0}': {1}".format(full_path, e.strerror),
-                                                  code=extension_error_code)
-
-                try:
-                    cg = CGroupConfigurator.for_extension(self.ext_handler.name)
-                    CGroupsTelemetry.track_extension(self.ext_handler.name, cg)
-                except Exception as e:
-                    self.logger.warn("Unable to setup cgroup {0}: {1}".format(self.ext_handler.name, e))
+                    raise ExtensionOperationError("Failed to launch '{0}': {1}".format(full_path, e.strerror), code=extension_error_code)
 
                 msg = ExtHandlerInstance._capture_process_output(process, stdout, stderr, cmd, timeout, extension_error_code)
 
