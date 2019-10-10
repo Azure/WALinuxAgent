a472bf7b69c2247a95eee0901b497cfc681317d2
larohra <41490930+larohra@users.noreply.github.com>
2019-10-10 13:58:06 -0700

    Remove agent version from environment variables (#1660)
    * Remove GAVersion Environment variable from the list
    
    * Removing unused variable
    
    * Adding a test case to check if all helper environment variables are set on every launch_command call
    
    * Modified the test to throw if a helper variable is added/removed/modified in the agent code
    
    * Modified the test to fix certain mock failing scenarios
    
    * Fixed test for py2.6

--- azurelinuxagent/ga/exthandlers.py
+++ azurelinuxagent/ga/exthandlers.py
@@ -87,8 +87,6 @@ class ExtCommandEnvVariable(object):
     ExtensionVersion = "%s_EXTENSION_VERSION" % Prefix
     ExtensionSeqNumber = "ConfigSequenceNumber"  # At par with Windows Guest Agent
     UpdatingFromVersion = "%s_UPDATING_FROM_VERSION" % Prefix
-    CurrentAgentVersion = "%s_CURRENT_VERSION" % Prefix
-
 
 
 def get_traceback(e):
@@ -1223,7 +1221,6 @@ class ExtHandlerInstance(object):
                 # Always add Extension Path and version to the current launch_command (Ask from publishers)
                 env.update({ExtCommandEnvVariable.ExtensionPath: base_dir,
                             ExtCommandEnvVariable.ExtensionVersion: str(self.ext_handler.properties.version),
-                            ExtCommandEnvVariable.CurrentAgentVersion: str(GOAL_STATE_AGENT_VERSION),
                             ExtCommandEnvVariable.ExtensionSeqNumber: str(self.get_seq_no())})
 
                 try:
