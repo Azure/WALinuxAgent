90d69fb56bbe61e56c670b66bbe8f597aad7782c
Norberto Arrieta <narrieta@users.noreply.github.com>
2019-10-10 18:09:09 -0700

    Update agent version to 2.2.43.2 (#1665)

--- azurelinuxagent/common/version.py
+++ azurelinuxagent/common/version.py
@@ -119,7 +119,7 @@ def get_distro():
 
 AGENT_NAME = "WALinuxAgent"
 AGENT_LONG_NAME = "Azure Linux Agent"
-AGENT_VERSION = '2.2.43.1'
+AGENT_VERSION = '2.2.43.2'
 AGENT_LONG_VERSION = "{0}-{1}".format(AGENT_NAME, AGENT_VERSION)
 AGENT_DESCRIPTION = """
 The Azure Linux Agent supports the provisioning and running of Linux
