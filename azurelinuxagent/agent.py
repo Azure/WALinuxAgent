5b2a8c1c1585f2dddfb498592118fe6062400fb9
remi.pauchet <remi.pauchet@stormshield.eu>
2019-11-20 10:56:35 +0100

    Add options for log dir and log rotation

--- azurelinuxagent/agent.py
+++ azurelinuxagent/agent.py
@@ -62,8 +62,23 @@ class Agent(object):
         #Init log
         verbose = verbose or conf.get_logs_verbose()
         level = logger.LogLevel.VERBOSE if verbose else logger.LogLevel.INFO
+        log_dir = conf.get_logs_dir()
+        try:
+            if os.path.isfile(log_dir):
+                raise Exception("{0} is a file".format(log_dir))
+            if not os.path.isdir(log_dir):
+                fileutil.mkdir(log_dir, mode=0o755, owner="root")
+        except Exception as e:
+            logger.error(
+                "Exception occurred while creating "
+                "log directory {0}: {1}".format(log_dir, e))
+        log_file = os.path.join(log_dir, "waagent.log")
+        log_max_bytes = conf.get_logs_rotation_max_bytes()
+        log_backup_count = conf.get_logs_rotation_backup_count()
         logger.add_logger_appender(logger.AppenderType.FILE, level,
-                                 path="/var/log/waagent.log")
+                                   path=log_file,
+                                   max_bytes=log_max_bytes,
+                                   backup_count=log_backup_count)
         if conf.get_logs_console():
             logger.add_logger_appender(logger.AppenderType.CONSOLE, level,
                     path="/dev/console")
