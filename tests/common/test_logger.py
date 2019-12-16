e6b9103c483a54948626316351385e58fd685755
larohra <larohra@microsoft.com>
2019-12-15 21:09:45 -0800

    Fixed test to check for MS

--- tests/common/test_logger.py
+++ tests/common/test_logger.py
@@ -191,26 +191,29 @@ class TestLogger(AgentTestCase):
             # If the time difference is > 5secs, there's a high probability that the time_in_file is in different TZ
             self.assertTrue((time_in_file-before_write_utc) <= timedelta(seconds=5))
 
-    def test_logger_should_log_micro_seconds(self):
+    @patch("azurelinuxagent.common.logger.datetime")
+    def test_logger_should_log_micro_seconds(self, mock_dt):
+        # datetime.isoformat() skips ms if ms=0, this test ensures that ms is always set
+
         file_name = "test.log"
         file_path = os.path.join(self.tmp_dir, file_name)
         test_logger = logger.Logger()
         test_logger.add_appender(logger.AppenderType.FILE, logger.LogLevel.INFO, path=file_path)
 
-        # datetime.isoformat() skips ms if ms=0, this test ensures that ms is always set
         ts_with_no_ms = datetime.utcnow().replace(microsecond=0)
+        mock_dt.utcnow = MagicMock(return_value=ts_with_no_ms)
 
-        with patch("datetime.utcnow", side_effect=ts_with_no_ms):
-            with open(file_path, "r") as log_file:
-                test_logger.info("The time should contain milli-seconds")
-                log = log_file.read()
-                try:
-                    time_in_file = datetime.strptime(log.split(logger.LogLevel.STRINGS[logger.LogLevel.INFO])[0].strip()
-                                                     , u'%Y-%m-%dT%H:%M:%S.%fZ')
-                except ValueError:
-                    self.fail("Ensure timestamp follows ISO-8601 format + 'Z' for UTC")
+        test_logger.info("The time should contain milli-seconds")
 
-                self.assertEqual(ts_with_no_ms, time_in_file, "Timestamps dont match")
+        with open(file_path, "r") as log_file:
+            log = log_file.read()
+            try:
+                time_in_file = datetime.strptime(log.split(logger.LogLevel.STRINGS[logger.LogLevel.INFO])[0].strip()
+                                                 , u'%Y-%m-%dT%H:%M:%S.%fZ')
+            except ValueError:
+                self.fail("Ensure timestamp follows ISO-8601 format and has micro seconds in it")
+
+            self.assertEqual(ts_with_no_ms, time_in_file, "Timestamps dont match")
 
     def test_telemetry_logger(self):
         mock = MagicMock()
