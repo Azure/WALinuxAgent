6516ce0f5512276cd03895266b382e7912461442
larohra <larohra@microsoft.com>
2019-12-15 20:05:54 -0800

    Modified log time format

--- azurelinuxagent/common/logger.py
+++ azurelinuxagent/common/logger.py
@@ -127,7 +127,7 @@ class Logger(object):
         else:
             msg = msg_format
             # This format is based on ISO-8601, Z represents UTC (Zero offset)
-        time = datetime.utcnow().isoformat() + "Z"
+        time = datetime.utcnow().strftime(u'%Y-%m-%dT%H:%M:%S.%fZ')
         level_str = LogLevel.STRINGS[level]
         if self.prefix is not None:
             log_item = u"{0} {1} {2} {3}\n".format(time, level_str, self.prefix,
