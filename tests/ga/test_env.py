53c9b671855d828495f7374dbebf4afbf5912d99
Paula Gombar <gombarica@gmail.com>
2020-07-22 16:12:41 -0700

    add output of os.environ

--- tests/ga/test_env.py
+++ tests/ga/test_env.py
@@ -18,6 +18,8 @@ from azurelinuxagent.common.osutil.default import DefaultOSUtil, shellutil
 from azurelinuxagent.ga.env import EnvHandler
 from tests.tools import AgentTestCase, patch
 
+import os
+
 
 class TestEnvHandler(AgentTestCase):
     def setUp(self):
@@ -37,6 +39,10 @@ class TestEnvHandler(AgentTestCase):
     def tearDown(self):
         self.mock_get_osutil.stop()
         AgentTestCase.tearDown(self)
+
+    def test_print_os_environment(self):
+        print(os.environ)
+        self.fail("TEST MESSAGE")
 
     def test_get_dhcp_client_pid_should_return_a_sorted_list_of_pids(self):
         with patch("azurelinuxagent.common.utils.shellutil.run_command", return_value="11 9 5 22 4 6"):
