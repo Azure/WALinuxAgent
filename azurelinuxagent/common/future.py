a6b00c9c0cf59ef01d4cb231538aa568e67f197e
Kevin Clark <kevin.clark@microsoft.com>
2020-08-20 10:34:56 -0700

    Enabled linter on loose files in common. (#1976)
    * Enabled linter on loose files in common.
    
    * Fixed removed comment from merge issue.
    
    * Removed ignore, allowing all files in common to be linted.
    
    Co-authored-by: keclar <keclar@microsoft.com>

--- azurelinuxagent/common/future.py
+++ azurelinuxagent/common/future.py
@@ -6,38 +6,40 @@ import re
 # Note broken dependency handling to avoid potential backward
 # compatibility issues on different distributions
 try:
-    import distro
+    import distro # pylint: disable=E0401
 except Exception:
     pass
 
+# pylint: disable=W0105
 """
 Add alias for python2 and python3 libs and functions.
-"""
+""" 
+# pylint: enable=W0105
 
 if sys.version_info[0] == 3:
-    import http.client as httpclient
-    from urllib.parse import urlparse
+    import http.client as httpclient # pylint: disable=W0611,import-error
+    from urllib.parse import urlparse # pylint: disable=W0611,import-error,no-name-in-module
 
-    """Rename Python3 str to ustr"""
-    ustr = str
+    """Rename Python3 str to ustr""" # pylint: disable=W0105
+    ustr = str # pylint: disable=C0103
 
-    bytebuffer = memoryview
+    bytebuffer = memoryview # pylint: disable=C0103
 
-    from collections import OrderedDict
+    from collections import OrderedDict # pylint: disable=W0611
 
 elif sys.version_info[0] == 2:
-    import httplib as httpclient
-    from urlparse import urlparse
+    import httplib as httpclient # pylint: disable=E0401,W0611
+    from urlparse import urlparse # pylint: disable=E0401
 
-    """Rename Python2 unicode to ustr"""
-    ustr = unicode
+    """Rename Python2 unicode to ustr""" # pylint: disable=W0105
+    ustr = unicode # pylint: disable=E0602,invalid-name
 
-    bytebuffer = buffer
+    bytebuffer = buffer # pylint: disable=E0602,invalid-name
 
     if sys.version_info[1] >= 7:
-        from collections import OrderedDict  # For Py 2.7+
+        from collections import OrderedDict  # For Py 2.7+ # pylint: disable=C0412
     else:
-        from ordereddict import OrderedDict  # Works only on 2.6
+        from ordereddict import OrderedDict  # Works only on 2.6 # pylint: disable=E0401
 else:
     raise ImportError("Unknown python version: {0}".format(sys.version_info))
 
@@ -46,9 +48,9 @@ def get_linux_distribution(get_full_name, supported_dists):
     """Abstract platform.linux_distribution() call which is deprecated as of
        Python 3.5 and removed in Python 3.7"""
     try:
-        supported = platform._supported_dists + (supported_dists,)
+        supported = platform._supported_dists + (supported_dists,) # pylint: disable=W0212
         osinfo = list(
-            platform.linux_distribution(
+            platform.linux_distribution( # pylint: disable=W1505
                 full_distribution_name=get_full_name,
                 supported_dists=supported
             )
@@ -61,7 +63,7 @@ def get_linux_distribution(get_full_name, supported_dists):
 
         if not osinfo or osinfo == ['', '', '']:
             return get_linux_distribution_from_distro(get_full_name)
-        full_name = platform.linux_distribution()[0].strip()
+        full_name = platform.linux_distribution()[0].strip() # pylint: disable=W1505
         osinfo.append(full_name)
     except AttributeError:
         return get_linux_distribution_from_distro(get_full_name)
@@ -93,7 +95,7 @@ def get_openwrt_platform():
     openwrt_version = re.compile(r"^DISTRIB_RELEASE=['\"](\d+\.\d+.\d+)['\"]")
     openwrt_product = re.compile(r"^DISTRIB_ID=['\"]([\w-]+)['\"]")
 
-    with open('/etc/openwrt_release', 'r') as fh:
+    with open('/etc/openwrt_release', 'r') as fh: # pylint: disable=C0103
         content = fh.readlines()
         for line in content:
             version_matches = openwrt_version.match(line)
