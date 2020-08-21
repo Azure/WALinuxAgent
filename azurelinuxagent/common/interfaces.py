3e0c9668cfcd5ccb7a89cb018d9aebc67470974a
larohra <larohra@microsoft.com>
2020-08-21 16:31:53 -0700

    Added interface for thread handlers

---
+++ azurelinuxagent/common/interfaces.py
@@ -0,0 +1,23 @@
+class ThreadHandlerInterface(object):
+    """
+    Interface for all thread handlers created and maintained by the GuestAgent.
+    """
+
+    @staticmethod
+    def get_thread_name():
+        raise NotImplementedError("get_thread_name() not implemented")
+
+    def run(self):
+        raise NotImplementedError("run() not implemented")
+
+    def is_alive(self):
+        raise NotImplementedError("is_alive() not implemented")
+
+    def start(self):
+        raise NotImplementedError("start() not implemented")
+
+    def stop(self):
+        raise NotImplementedError("stop() not implemented")
+
+    def stopped(self):
+        raise NotImplementedError("stopped() not implemented")
