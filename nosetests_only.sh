214af2f76f2e266d96819fcb36f42ce187922171
keclar <keclar@microsoft.com>
2020-08-21 22:20:27 +0000

    Swapped ordering of sudo and non-sudo tests

--- nosetests_only.sh
+++ nosetests_only.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-echo "========================================="
-echo "nosetests -a 'requires_sudo' output"
-echo "========================================="
-sudo env "PATH=$PATH" nosetests -a 'requires_sudo' tests
-
 echo
 echo "========================================="
 echo "nosetests -a '!requires_sudo' output"
 echo "========================================="
 nosetests -a '!requires_sudo' tests
+
+echo "========================================="
+echo "nosetests -a 'requires_sudo' output"
+echo "========================================="
+sudo env "PATH=$PATH" nosetests -a 'requires_sudo' tests
