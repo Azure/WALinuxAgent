38c4e86e71659ac4b4e5a7dec5f739f82110d68d
larohra <larohra@microsoft.com>
2020-09-21 11:41:20 -0700

    Hijacking verbose flag for testing

--- azurelinuxagent/ga/exthandlers.py
+++ azurelinuxagent/ga/exthandlers.py
@@ -84,7 +84,7 @@ _STATUS_FILE_RETRY_DELAY = 2  # seconds
 _ENABLE_EXTENSION_TELEMETRY_PIPELINE = False
 
 def is_extension_telemetry_pipeline_enabled():
-    return _ENABLE_EXTENSION_TELEMETRY_PIPELINE
+    return conf.get_logs_verbose()
 
 
 class ValidHandlerStatus(object): # pylint: disable=R0903
