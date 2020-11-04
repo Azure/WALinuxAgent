06229ba5d9d4bc26937881ce637c4d86e69b9c8b
Paula Gombar <gombarica@gmail.com>
2020-11-04 11:13:12 -0800

    Revert "Gs fix for no config (#2016)"
    This reverts commit 7c2141494203e71c7163787f11a61656be29da20.

--- azurelinuxagent/common/exception.py
+++ azurelinuxagent/common/exception.py
@@ -153,10 +153,6 @@ class ProtocolNotFoundError(ProtocolError):
     def __init__(self, msg=None, inner=None): # pylint: disable=W0235
         super(ProtocolNotFoundError, self).__init__(msg, inner)
 
-class IncompleteGoalStateError(ProtocolError):
-    """
-    Goal state is returned incomplete.
-    """
 
 class HttpError(AgentError):
     """
