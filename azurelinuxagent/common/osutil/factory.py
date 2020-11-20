35969a292194b8a28247c4ecc5bf265a8b278260
Paula Gombar <gombarica@gmail.com>
2020-11-19 16:16:42 -0800

    fix remaining pylint warning

--- azurelinuxagent/common/osutil/factory.py
+++ azurelinuxagent/common/osutil/factory.py
@@ -61,15 +61,15 @@ def _get_osutil(distro_name, distro_code_name, distro_version, distro_full_name)
     if distro_name == "ubuntu":
         if Version(distro_version) in [Version("12.04"), Version("12.10")]:
             return Ubuntu12OSUtil()
-        elif Version(distro_version) in [Version("14.04"), Version("14.10")]:
+        if Version(distro_version) in [Version("14.04"), Version("14.10")]:
             return Ubuntu14OSUtil()
-        elif Version(distro_version) in [Version('16.04'), Version('16.10'), Version('17.04')]:
+        if Version(distro_version) in [Version('16.04'), Version('16.10'), Version('17.04')]:
             return Ubuntu16OSUtil()
-        elif Version(distro_version) in [Version('18.04'), Version('18.10'),
+        if Version(distro_version) in [Version('18.04'), Version('18.10'),
                                          Version('19.04'), Version('19.10'),
                                          Version('20.04')]:
             return Ubuntu18OSUtil()
-        elif distro_full_name == "Snappy Ubuntu Core":
+        if distro_full_name == "Snappy Ubuntu Core":
             return UbuntuSnappyOSUtil()
 
         return UbuntuOSUtil()
