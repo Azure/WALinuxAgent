e757533ddee855a02366fb5e7865da4e1439c743
keclar <keclar@microsoft.com>
2020-12-10 16:12:50 -0800

    Added python3.9 support and simplified feature to only include what Travis CI supported before.

--- ci/nosetests.sh
+++ ci/nosetests.sh
@@ -7,15 +7,15 @@ EXIT_CODE=0
 echo "========================================="
 echo "nosetests -a '!requires_sudo' output"
 echo "========================================="
-nosetests -a '!requires_sudo' tests $(echo "${NOSEOPTS/__uuid__/$(uuidgen)}") || EXIT_CODE=$(($EXIT_CODE || $?))
-echo EXIT_CODE nosetests = $EXIT_CODE
-sudo mv .coverage coverage.$(uuidgen).no_requires_sudo.data
+nosetests -a '!requires_sudo' tests $NOSEOPTS || EXIT_CODE=$(($EXIT_CODE || $?))
+echo EXIT_CODE no_sudo nosetests = $EXIT_CODE
+sudo mv .coverage coverage.$(uuidgen).no_sudo.data
 
 echo "========================================="
 echo "nosetests -a 'requires_sudo' output"
 echo "========================================="
-sudo env "PATH=$PATH" nosetests -a 'requires_sudo' tests $(echo "${NOSEOPTS/__uuid__/$(uuidgen)}") || EXIT_CODE=$(($EXIT_CODE || $?))
-echo EXIT_CODE sudo nosetest = $EXIT_CODE
-sudo mv .coverage coverage.$(uuidgen).requires_sudo.data
+sudo env "PATH=$PATH" nosetests -a 'requires_sudo' tests $NOSEOPTS || EXIT_CODE=$(($EXIT_CODE || $?))
+echo EXIT_CODE with_sudo nosetests = $EXIT_CODE
+sudo mv .coverage coverage.$(uuidgen).with_sudo.data
 
 exit "$EXIT_CODE"
