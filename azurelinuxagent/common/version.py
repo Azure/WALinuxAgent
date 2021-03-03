6778c0e9d5d6cb918861fc214a24b961fde7c703
Kevin Clark <kevin.clark@microsoft.com>
2021-03-03 10:42:00 -0800

    Increase Agent Version to 2.2.54.1 and updated test zips (#2194)

--- azurelinuxagent/common/version.py
+++ azurelinuxagent/common/version.py
@@ -196,7 +196,7 @@ def has_logrotate():
 
 AGENT_NAME = "WALinuxAgent"
 AGENT_LONG_NAME = "Azure Linux Agent"
-AGENT_VERSION = '2.2.54'
+AGENT_VERSION = '2.2.54.1'
 AGENT_LONG_VERSION = "{0}-{1}".format(AGENT_NAME, AGENT_VERSION)
 AGENT_DESCRIPTION = """
 The Azure Linux Agent supports the provisioning and running of Linux
