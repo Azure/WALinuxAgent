da5bb26e7cf8450233836fadb0f474ae65e1d298
Bryan Yue <yue_bryan123@hotmail.com>
2021-03-11 17:01:58 -0800

    fix

--- azurelinuxagent/common/utils/restutil.py
+++ azurelinuxagent/common/utils/restutil.py
@@ -140,9 +140,10 @@ def _compute_delay(retry_attempt=1, delay=DELAY_IN_SECONDS):
     # Binet's Formula for Nth Fibonacci Term:
     # https://artofproblemsolving.com/wiki/index.php/Binet%27s_Formula
     sqrt5 = math.sqrt(5)
+    # Add 2 to be consistent with previous iterative approach indexing
+    n = retry_attempt + 2
     fib_n = int(round((( (1 + sqrt5) ** retry_attempt - (1 - sqrt5) ** retry_attempt ) / ( 2 ** retry_attempt * sqrt5 ))))
-    # Add 2 to be equivalent with previous iterative approach indexing
-    return delay * (fib_n + 2)
+    return delay * fib_n
 
 
 def _is_retry_status(status, retry_codes=None):
