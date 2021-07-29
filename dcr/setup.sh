821bb0245b5af957f8975c1547e4a364cec45da4
larohra <41490930+larohra@users.noreply.github.com>
2021-07-29 14:51:12 -0700

    always ensure agent version is set

--- dcr/setup.sh
+++ dcr/setup.sh
@@ -7,7 +7,7 @@ set -euxo pipefail
 
 # Copy agent zip file to /var/lib/waagent to force it to autoupdate
 echo "PWD: $(pwd)"
-version=$1
+[  -z "$1" ] && version="9.9.9.9" || version=$1
 
 # Required for Agent-BVT test
 echo "$version" > /etc/agent-release
@@ -16,3 +16,4 @@ sudo systemctl stop walinuxagent
 sudo cp ./*-$version.zip /var/lib/waagent
 sudo unzip /var/lib/waagent/WALinuxAgent-$version.zip -d /var/lib/waagent/WALinuxAgent-$version
 sudo systemctl daemon-reload && sudo systemctl start walinuxagent
+waagent --version
