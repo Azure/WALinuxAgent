c4fe664621d2ef71805fa26c705ac6890129df5c
larohra <41490930+larohra@users.noreply.github.com>
2021-07-30 09:59:28 -0700

    Add tests

--- dcr/agent-bvt/test_waagent_log.py
+++ dcr/agent-bvt/test_waagent_log.py
@@ -8,7 +8,7 @@ def test_agent_log():
     errors = []
     with open(agent_log_file, "r") as log_file:
         for line in log_file.readlines():
-            if "error" in line.lower():
+            if "ERROR" in line:
                 errors.append(line.strip())
 
     assert len(errors) == 0, "Errors found in log:\n{0}".format('\n'.join(errors))
