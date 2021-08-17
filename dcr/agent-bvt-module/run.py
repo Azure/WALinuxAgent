bc91207bb052058896954cbc8ab758d0e8d43582
larohra <41490930+larohra@users.noreply.github.com>
2021-08-17 15:38:55 -0700

    Fixed bad py file

--- dcr/agent-bvt-module/run.py
+++ dcr/agent-bvt-module/run.py
@@ -1,7 +1,12 @@
+import os
+
 from test_agent_version import test_agent_version
 from run_cse_tests import execute_cse_tests
 
 if __name__ == '__main__':
+    # Emviron vars
+    print(os.environ)
+
     # Test 1
     stdout, stderr = test_agent_version()
     print("Stdout: {0}\nStderr: {1}".format(stdout, stderr))
