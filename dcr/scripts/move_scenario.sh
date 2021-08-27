4cae21b2983d817165d1ddbb2e057155696b37ce
larohra <41490930+larohra@users.noreply.github.com>
2021-08-27 10:38:11 -0700

    Try dynamic template

--- dcr/scripts/move_scenario.sh
+++ dcr/scripts/move_scenario.sh
@@ -6,10 +6,9 @@ set -euxo pipefail
 shopt -s extglob
 pushd "$BUILD_SOURCESDIRECTORY/dcr/scenarios"
 rm -rf !("$SCENARIONAME")
-ls -al
 popd
 
 # Move contents of the remaining scenario to a directory called scenario
 # This is done to be able to import the yml easily as importing a yml template can only be static, it cant be dynamic
 mkdir "$BUILD_SOURCESDIRECTORY/dcr/scenario"
-cp -r "$BUILD_SOURCESDIRECTORY/dcr/scenarios/$SCENARIONAME/*" "$BUILD_SOURCESDIRECTORY/dcr/scenario/"
+mv "$BUILD_SOURCESDIRECTORY/dcr/scenarios/$SCENARIONAME"/* "$BUILD_SOURCESDIRECTORY/dcr/scenario/"
