410f7c7b9b1e93a81b8f66b296f835a228432428
larohra <41490930+larohra@users.noreply.github.com>
2021-08-31 20:18:11 -0700

    Fixed bad name

--- dcr/scripts/run_pytest.sh
+++ dcr/scripts/run_pytest.sh
@@ -6,4 +6,4 @@ set -euxo pipefail
 #           $1          $2          $3            $4              $5            $6                    $7
 # Usage:  Artifact Dir
 
-pytest ./**/agent-bvt/ --doctest-modules --junitxml="$1/test-result-$SCENARIONAME.xml"
+pytest ./**/agent-bvt/ --doctest-modules --junitxml="$1/test-result.xml"
