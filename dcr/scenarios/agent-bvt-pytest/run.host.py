125af2652be9e5f146108ecc439ec7386e6dde4a
larohra <41490930+larohra@users.noreply.github.com>
2021-09-01 11:19:41 -0700

    run python script using script

--- dcr/scenarios/agent-bvt-pytest/run.host.py
+++ dcr/scenarios/agent-bvt-pytest/run.host.py
@@ -1,10 +1,13 @@
 import subprocess
 
 import os
+import sys
 
 from dcr.scenario_utils.common_utils import execute_command_and_raise_on_error
 
 if __name__ == '__main__':
+    print(sys.executable)
+    print(os.environ)
     username = os.environ["ADMINUSERNAME"]
     host = os.environ["ARMDEPLOYMENTOUTPUT_HOSTNAME_VALUE"]
     stdout, stderr = execute_command_and_raise_on_error('ssh -o StrictHostKeyChecking=no {0}@{1} "sudo bash /home/{0}/dcr/scripts/run_pytest.sh /home/{0}/test_results"'.format(username, host), shell=True)
