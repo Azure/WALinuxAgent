993f4d256bdff0b0e5fdeac8658d61c3ea87e11b
larohra <41490930+larohra@users.noreply.github.com>
2021-09-01 09:29:04 -0700

    try scenario utils

--- dcr/scenarios/agent-bvt-pytest/run.host.py
+++ dcr/scenarios/agent-bvt-pytest/run.host.py
@@ -2,10 +2,9 @@ import subprocess
 
 import os
 
+from dcr.scenario_utils.common_utils import execute_command_and_raise_on_error
+
 if __name__ == '__main__':
     username = os.environ["ADMINUSERNAME"]
     host = os.environ["ARMDEPLOYMENTOUTPUT_HOSTNAME_VALUE"]
-    popen = subprocess.Popen('ssh -o StrictHostKeyChecking=no {0}@{1} "sudo bash /home/{0}/dcr/scripts/run_pytest.sh /home/{0}/test_results"'.format(username, host), shell=True)
-    stdout, stderr = popen.communicate(timeout=5 * 60)
-
-    print("RC: {0}\n\tSTDOUT: {1}\n\tSTDERR: {2}".format(popen.returncode, stdout, stderr))
+    stdout, stderr = execute_command_and_raise_on_error('ssh -o StrictHostKeyChecking=no {0}@{1} "sudo bash /home/{0}/dcr/scripts/run_pytest.sh /home/{0}/test_results"'.format(username, host), shell=True)
