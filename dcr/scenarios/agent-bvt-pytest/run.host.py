f48ac1f5f1a195a0f63af18982720bc6bb8ee1a3
larohra <41490930+larohra@users.noreply.github.com>
2021-08-31 18:16:36 -0700

    just host test

--- dcr/scenarios/agent-bvt-pytest/run.host.py
+++ dcr/scenarios/agent-bvt-pytest/run.host.py
@@ -5,7 +5,7 @@ import os
 if __name__ == '__main__':
     username = os.environ["ADMINUSERNAME"]
     host = os.environ["ARMDEPLOYMENTOUTPUT_HOSTNAME_VALUE"]
-    popen = subprocess.Popen('ssh -o StrictHostKeyChecking=no {0}@{1} "sudo bash /home/{0}/dcr/scripts/run_pytest.sh /home/{0}/test_results"'.format(username, host))
+    popen = subprocess.Popen('ssh -o StrictHostKeyChecking=no {0}@{1} "sudo bash /home/{0}/dcr/scripts/run_pytest.sh /home/{0}/test_results"'.format(username, host), shell=True)
     stdout, stderr = popen.communicate(timeout=5 * 60)
 
     print("RC: {0}\n\tSTDOUT: {1}\n\tSTDERR: {2}".format(popen.returncode, stdout, stderr))
