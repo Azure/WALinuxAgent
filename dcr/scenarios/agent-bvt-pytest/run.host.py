59e29208f08d1f57d9e6fbe5893744aeaa826f6b
larohra <41490930+larohra@users.noreply.github.com>
2021-09-01 12:54:48 -0700

    Change to py3.7

--- dcr/scenarios/agent-bvt-pytest/run.host.py
+++ dcr/scenarios/agent-bvt-pytest/run.host.py
@@ -10,4 +10,6 @@ if __name__ == '__main__':
     print(os.environ)
     username = os.environ["ADMINUSERNAME"]
     host = os.environ["ARMDEPLOYMENTOUTPUT_HOSTNAME_VALUE"]
-    stdout, stderr = execute_command_and_raise_on_error('ssh -o StrictHostKeyChecking=no {0}@{1} "sudo bash /home/{0}/dcr/scripts/run_pytest.sh /home/{0}/test_results"'.format(username, host), shell=True)
+    pypy_path = os.environ["AGENTZIP_PYPYPATH"]
+
+    stdout, stderr = execute_command_and_raise_on_error('ssh -o StrictHostKeyChecking=no {0}@{1} "sudo bash PYPYPATH={2} /home/{0}/dcr/scripts/run_pytest.sh /home/{0}/test_results"'.format(username, host, pypy_path), shell=True)
