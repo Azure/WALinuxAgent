92481fe7e4d2b46e2680bb8f61c5264c46d6b272
larohra <41490930+larohra@users.noreply.github.com>
2021-09-08 16:58:58 -0700

    Dont fail if test result file not found on host

--- dcr/scripts/setup_agent.sh
+++ dcr/scripts/setup_agent.sh
@@ -20,6 +20,8 @@ sudo systemctl stop $agent
 
 # We need to force the agent to AutoUpdate to enable our testing
 sed -i 's/AutoUpdate.Enabled=n/AutoUpdate.Enabled=y/g' /etc/waagent.conf
+# Move the older agent log file to ensure we have a clean slate when testing agent logs
+mv /var/log/waagent.log /var/log/waagent.old.log
 
 sudo cp -r ./dcr/*-$version /var/lib/waagent
 sudo systemctl daemon-reload && sudo systemctl start $agent
