627b7a2ee8678f8b3acfdd54b001d73152531fa6
larohra <41490930+larohra@users.noreply.github.com>
2021-09-10 13:44:29 -0700

    Added asyncio

---
+++ dcr/scripts/orchestrator/execute_ssh_on_vm.py
@@ -0,0 +1,47 @@
+import asyncio
+import logging
+
+import os
+import sys
+
+import uuid
+
+logger = logging.getLogger(__name__)
+
+
+async def execute_command_concurrently(command, username, ip, max_retry=5):
+    ssh_cmd = f'ssh -o StrictHostKeyChecking=no {username}@{ip} "{command}"'
+    attempt = 0
+    while attempt < max_retry:
+        try:
+            proc = await asyncio.create_subprocess_shell(ssh_cmd, stdout=asyncio.subprocess.PIPE,
+                                                         stderr=asyncio.subprocess.PIPE)
+            stdout, stderr = await proc.communicate()
+            if proc.returncode != 0:
+                raise Exception(
+                    f"Command {ssh_cmd} failed with exit code: {proc.returncode}.\n\tStdout: {stdout}\n\tStderr: {stderr}")
+            return stdout, stderr
+        except Exception as err:
+            attempt += 1
+            if attempt < max_retry:
+                logger.warning(f"({attempt}/{max_retry}) Failed to execute command: {err}. Retrying in 3 secs",
+                               exc_info=True)
+                await asyncio.sleep(3)
+            else:
+                raise
+
+
+async def run_tasks(username, ips):
+    tasks = [asyncio.create_task(
+        execute_command_concurrently(username=username, command=f"echo yolo-{uuid.uuid4()}", ip=ip_)) for ip_ in
+        ips.split(",")]
+
+    return await asyncio.gather(*tasks, return_exceptions=True)
+
+
+if __name__ == '__main__':
+    admin_username = os.environ['ADMINUSERNAME']
+    vm_ips = os.environ['ARMDEPLOYMENTOUTPUT_HOSTNAME_VALUE']
+    print(sys.argv)
+
+    print(asyncio.run(run_tasks(admin_username, vm_ips)))
