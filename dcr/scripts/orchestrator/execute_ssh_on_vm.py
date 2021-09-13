598f4e126fbffa47a244f24cd960ee75c26e3ef4
larohra <41490930+larohra@users.noreply.github.com>
2021-09-13 09:34:05 -0700

    Test if cancellation throws

--- dcr/scripts/orchestrator/execute_ssh_on_vm.py
+++ dcr/scripts/orchestrator/execute_ssh_on_vm.py
@@ -11,14 +11,24 @@ logger = logging.getLogger(__name__)
 
 
 async def execute_command_concurrently(command, username, ip, max_retry=5):
-    ssh_cmd = f'ssh -o StrictHostKeyChecking=no {username}@{ip} "{command}"'
+    dcr_root_dir = f"/home/{username}/dcr"
+    pypy_path = os.environ.get("PYPYPATH")
+    ssh_cmd = f'ssh -o StrictHostKeyChecking=no {username}@{ip}'
+    agent_version = os.environ.get("AGENTVERSION")
+
+    commands_to_execute = [
+        f"scp -o StrictHostKeyChecking=no -r {os.environ.get('BUILD_SOURCESDIRECTORY')}/dcr/ {username}@${ip}:~/"
+        f'{ssh_cmd} "sudo PYPYPATH="{pypy_path}" bash {dcr_root_dir}/scripts/install_pip_packages.sh {dcr_root_dir}/requirements.txt"'
+        f'{ssh_cmd} "sudo bash {dcr_root_dir}/scripts/setup_agent.sh {agent_version}"'
+    ]
+
     attempt = 0
     # if max_retry % 2 == 0:
     #     raise Exception(f"Failing {ssh_cmd}")
 
-    await asyncio.sleep(10)
-    while attempt < max_retry:
+    while attempt < 1:
         try:
+            await asyncio.sleep(10)
             proc = await asyncio.create_subprocess_shell(ssh_cmd, stdout=asyncio.subprocess.PIPE,
                                                          stderr=asyncio.subprocess.PIPE)
             stdout, stderr = await proc.communicate()
