605a7254fead15dc985364b8e6277bbdce4d3864
larohra <41490930+larohra@users.noreply.github.com>
2021-09-23 14:15:42 +0530

    Added logging data

--- dcr/scenario_utils/logging_utils.py
+++ dcr/scenario_utils/logging_utils.py
@@ -7,4 +7,5 @@ class LoggingHandler:
     Base class for inheritance logging
     """
     def __init__(self):
+        logging.basicConfig(level=logging.DEBUG)
         self.log = logging.getLogger(self.__class__.__name__)
