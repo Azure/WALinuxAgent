4cf303cd3b37538a7640029d862c957c2b9e838c
larohra <41490930+larohra@users.noreply.github.com>
2021-09-29 19:08:07 +0530

    Fixed reboots

--- dcr/scenarios/agent-persist-firewall/setup.sh
+++ dcr/scenarios/agent-persist-firewall/setup.sh
@@ -14,7 +14,7 @@ mkdir -p /home/$username || echo "this is only needed for Suse VMs for running c
 # Setup Cron jobs
 echo "@reboot ($d --utc +\\%FT\\%T.\\%3NZ && /usr/bin/access_wire_ip.sh $ipt) > /var/tmp/reboot-cron-root.log 2>&1" | crontab -u root -
 echo "@reboot ($d --utc +\\%FT\\%T.\\%3NZ && /usr/bin/access_wire_ip.sh $ipt) > /var/tmp/reboot-cron-non-root.log 2>&1" | crontab -u $username -
-echo "@reboot ($d --utc +\\%FT\\%T.\\%3NZ) > /var/log/reboot_time.txt 2>&1)" | crontab -u root -
+(crontab -l 2>/dev/null; echo "@reboot ($d --utc +\%FT\%T.\%3NZ) > /var/log/reboot_time.txt 2>&1") | crontab -u root -
 s=$(which systemctl)
 echo "@reboot ($s status walinuxagent-network-setup.service || $s status waagent-network-setup.service) > /var/log/reboot_network_setup.txt 2>&1)" | crontab -u root -
 
