13c5c475bad448a6113b804b11e635f103832cd1
larohra <41490930+larohra@users.noreply.github.com>
2021-09-29 21:11:59 +0530

    Improved logger

--- dcr/scenario_utils/logging_utils.py
+++ dcr/scenario_utils/logging_utils.py
@@ -6,10 +6,31 @@ def get_logger(name):
     return LoggingHandler(name).log
 
 
+class MyFormatter(logging.Formatter):
+    def format(self, record):
+        record.statement = "debug" if record.levelname in (logging.INFO, logging.DEBUG) else record.levelname.lower()
+        return logging.Formatter.format(self, record)
+
+
 class LoggingHandler:
     """
     Base class for inheritance logging
     """
     def __init__(self, name=None):
         # logging.basicConfig(level=logging.INFO)
-        self.log = logging.getLogger(name if name is not None else self.__class__.__name__)
+        self.log = self.__setup_and_get_logger(name)
+
+    def __setup_and_get_logger(self, name):
+        logger = logging.getLogger(name if name is not None else self.__class__.__name__)
+        if logger.hasHandlers():
+            return logger
+
+        # No handlers found for logger, set it up
+        log_formatter = logging.Formatter("##[%(statement)s] [%(levelname)s] [%(asctime)s] [%(module)s] %(message)s")
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(log_formatter)
+        logger.addHandler(console_handler)
+        logger.setLevel(logging.INFO)
+
+        return logger
+
