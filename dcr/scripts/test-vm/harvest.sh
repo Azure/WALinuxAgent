0bbba17a5cac73b74f63092b1d79177d449a35f7
larohra <41490930+larohra@users.noreply.github.com>
2021-10-01 12:12:17 +0530

    Fixed harvest commands

--- dcr/scripts/test-vm/harvest.sh
+++ dcr/scripts/test-vm/harvest.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #            1          2           3
-# Usage: <username>    <IP>   <Artifact Directory>
+# Usage: <username>   <IP>  <Artifact Directory>
 set -euxo pipefail
 
-ssh -o "StrictHostKeyChecking no" "$1"@"$2" "sudo tar --exclude='/var/log/journal/' cfz logs-$2.tgz /var/log /var/lib/waagent/ /root"
+ssh -o "StrictHostKeyChecking no" "$1"@"$2" "sudo tar --exclude=/var/log/journal/ cfz logs-$2.tgz /var/log /var/lib/waagent/ /root"
 
 # Create directory if doesn't exist
 mkdir -p "$3"
-scp -o "StrictHostKeyChecking no" "$1"@"$2":"logs-$2.tgz" "$3"/"logs-$2.tgz"
+scp -o "StrictHostKeyChecking no" "$1@$2:logs-$2.tgz" "$3/logs-$2.tgz"
