64fbcead5710b18dbc64ab12fc5e93089809ba9e
dganesan <dhivyag1991@gmail.com>
2021-10-25 10:21:56 -0700

    made default python as 3 for mariner init

--- setup.py
+++ setup.py
@@ -138,7 +138,8 @@ def get_data_files(name, version, fullname):  # pylint: disable=R0912
         set_systemd_files(data_files, dest=systemd_dir_path,
                           src=["init/clearlinux/waagent.service"])
     elif name == 'mariner':
-        set_bin_files(data_files, dest=agent_bin_path)
+        set_bin_files(data_files, dest=agent_bin_path,
+                      src=["bin/py3/waagent", "bin/waagent2.0"])
         set_conf_files(data_files, dest="/etc",
                        src=["config/mariner/waagent.conf"])
         set_systemd_files(data_files, dest=systemd_dir_path,
