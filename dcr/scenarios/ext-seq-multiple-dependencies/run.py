9c7868eb02c4cde48cba5a9528b542775002cd59
Laveesh Rohra <larohra@microsoft.com>
2021-11-30 12:14:53 -0800

    Fix failing cases in DCR v2 (#2422)

--- dcr/scenarios/ext-seq-multiple-dependencies/run.py
+++ dcr/scenarios/ext-seq-multiple-dependencies/run.py
@@ -1,3 +1,5 @@
+import socket
+
 from dcr.scenario_utils.check_waagent_log import check_waagent_log_for_errors
 from dcr.scenario_utils.test_orchestrator import TestFuncObj, TestOrchestrator
 
@@ -9,5 +11,5 @@ if __name__ == '__main__':
 
     test_orchestrator = TestOrchestrator("ExtSeqDependency-VM", tests=tests)
     test_orchestrator.run_tests()
-    test_orchestrator.generate_report_on_vm("test-result-ext-seq-vm.xml")
+    test_orchestrator.generate_report_on_vm(f"test-result-ext-seq-vm-{socket.gethostname()}.xml")
     assert not test_orchestrator.failed, f"Test Suite: {test_orchestrator.name} failed"
