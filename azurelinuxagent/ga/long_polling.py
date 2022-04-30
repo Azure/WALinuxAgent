cd55aa83c7fc15e5a1d89cb24961db554231ae33
nnandigam <nnandigam@microsoft.com>
2022-04-29 17:58:49 -0700

    fix timeout error

--- azurelinuxagent/ga/long_polling.py
+++ azurelinuxagent/ga/long_polling.py
@@ -1,11 +1,10 @@
 import datetime
+import socket
 import threading
-import time
 
 from azurelinuxagent.common.interfaces import ThreadHandlerInterface
 from azurelinuxagent.common import logger
 from azurelinuxagent.common.future import httpclient, ustr
-from azurelinuxagent.common.utils import textutil
 
 
 def get_longpolling_handler():
@@ -52,24 +51,14 @@ class LongPollingHandler(ThreadHandlerInterface):
     def daemon(self):
         new_request = True
         while new_request:
+            logger.info("[HTTP Request(longPolling)] Timestamp:{0}".format(datetime.datetime.utcnow()))
+            conn = httpclient.HTTPConnection("168.63.129.16", 32526, timeout=60)
             try:
-                logger.info("[HTTP Request(longPolling)] Timestamp:{0}".format(datetime.datetime.utcnow()))
-
-                timeout = 60
-                conn = httpclient.HTTPConnection("168.63.129.16", 32526, timeout=10)
                 url = "/longPolling"
                 conn.request(method="GET", url=url)
-                resp = None
-
-                while timeout > 0 and resp is None:
-                    resp = conn.getresponse()
-                    time.sleep(1)
-                    timeout -= 1
-
-                if timeout == 0:
-                    logger.warn("[HTTP Request(longPolling)] timed out at : {0}".format(datetime.datetime.utcnow()))
-                else:
-                    if resp.status != 200 and not(500 <= resp.status <= 599):
+                resp = conn.getresponse()
+                if resp is not None:
+                    if resp.status != 200 and resp.status !=204 and not 500 <= resp.status <= 599:
                         new_request = False
                     result = "[HTTP Response(longPolling)] Timestamp:{0}; {1} [{2}: {3}] {4}".format(
                         datetime.datetime.utcnow(),
@@ -78,7 +67,10 @@ class LongPollingHandler(ThreadHandlerInterface):
                         resp.reason,
                         resp.read())
                     logger.info(result)
-
+            except socket.timeout as st:
+                logger.warn("[HTTP Request(longPolling)] timed out at : {0}".format(datetime.datetime.utcnow()))
             except Exception as e:
-                logger.error("Failed to fetch from longPolling: {0}".format(textutil.format_exception(e)))
+                logger.error("Failed to fetch from longPolling: {0}".format(ustr(e)))
                 new_request = False
+            finally:
+                conn.close()
