8a79ea77371859bde3ae1e0884c59f7530ea0d0d
Norberto Arrieta <narrieta@users.noreply.github.com>
2022-09-20 07:06:53 -0700

    Add logging statements for mrseq migration during update (#2667)
    Co-authored-by: narrieta <narrieta>

--- azurelinuxagent/ga/exthandlers.py
+++ azurelinuxagent/ga/exthandlers.py
@@ -1210,7 +1210,10 @@ class ExtHandlerInstance(object):
 
         old_ext_mrseq_file = os.path.join(old_ext_dir, "mrseq")
         if os.path.isfile(old_ext_mrseq_file):
+            logger.info("Migrating {0} to {1}.", old_ext_mrseq_file, new_ext_dir)
             shutil.copy2(old_ext_mrseq_file, new_ext_dir)
+        else:
+            logger.info("{0} does not exist, no migration is needed.", old_ext_mrseq_file)
 
         old_ext_status_dir = old_ext_handler_i.get_status_dir()
         new_ext_status_dir = self.get_status_dir()
