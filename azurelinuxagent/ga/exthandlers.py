a82427fc7012b133e5a799984d00e43f53ea2a9f
maddieford <93676569+maddieford@users.noreply.github.com>
2022-10-31 16:44:19 -0700

    Report message in handler heartbeat (#2688)
    * Report message in handler heartbeat
    
    * Check heartbeat message exists
    
    * Add unit test for heartbeat reporting
    
    * Fix unit test syntax error
    
    * Speed up test by removing call to sleep
    
    * Remove http_put_handler

--- azurelinuxagent/ga/exthandlers.py
+++ azurelinuxagent/ga/exthandlers.py
@@ -1001,6 +1001,8 @@ class ExtHandlersHandler(object):
                     heartbeat = ext_handler_i.collect_heartbeat()
                     if heartbeat is not None:
                         handler_status.status = heartbeat.get('status')
+                        if 'formattedMessage' in heartbeat:
+                            handler_status.message = parse_formatted_message(heartbeat.get('formattedMessage'))
                 except ExtensionError as e:
                     ext_handler_i.set_handler_status(message=ustr(e), code=e.code)
 
