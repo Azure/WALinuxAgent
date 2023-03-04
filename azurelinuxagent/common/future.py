d74e1b1ce4e074d07dcdc550cecf888ab02f9f4c
Nageswara Nandigam <nnandigam@microsoft.com>
2023-03-03 16:51:28 -0800

    fetch full distro version for mariner

--- azurelinuxagent/common/future.py
+++ azurelinuxagent/common/future.py
@@ -109,6 +109,12 @@ def get_linux_distribution_from_distro(get_full_name):
     )
     full_name = distro.linux_distribution()[0].strip()
     osinfo.append(full_name)
+
+    # Fixing is the problem https://github.com/Azure/WALinuxAgent/issues/2715. Distro.linux_distribution method not retuning full version
+    # If best is true, the most precise version number out of all examined sources is returned.
+    if osinfo and "mariner" in osinfo[0].lower():
+        osinfo[1] = distro.version(best=True)
+
     return osinfo
 
 
