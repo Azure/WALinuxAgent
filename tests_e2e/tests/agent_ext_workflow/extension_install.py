5aad0ac1e39ef69f13dffca6f1613b7caf19a4e2
Maddie Ford (SHE/HER) <maddieford@microsoft.com>
2023-05-22 13:20:56 -0700

    Assert ext is installed

--- tests_e2e/tests/agent_ext_workflow/extension_install.py
+++ tests_e2e/tests/agent_ext_workflow/extension_install.py
@@ -35,21 +35,47 @@ from tests_e2e.tests.lib.vm_extension import VmExtension
 
 
 class ExtensionInstall(AgentTest):
+    class GuestAgentDcrTestExtension:
+        COUNT_KEY_NAME = "Count"
+        NAME_KEY_NAME = "name"
+
+        def __init__(self, extension: VmExtension):
+            self.extension = extension
+            self.name = "GuestAgentDcr-TestInstall"
+            self.version = "1.1.5"
+            self.message = ""
+            self.enable_count = 0
+
+        def modify_ext_settings_and_enable(self):
+            self.enable_count += 1
+            setting_name = "%s-%s, %s: %s" % (self.name, self.version, self.COUNT_KEY_NAME, self.enable_count)
+            settings = {self.NAME_KEY_NAME: setting_name.encode('utf-8')}
+            self.extension.enable(settings=settings, auto_upgrade_minor_version=False)
+            self.message = settings
+
+        def assert_instance_view(self):
+            self.extension.assert_instance_view(expected_version=self.version, expected_message=self.message)
+
     def run(self):
         ssh_client: SshClient = self._context.create_ssh_client()
 
         is_arm64: bool = ssh_client.get_architecture() == "aarch64"
 
+        # create extension abstraction
         dcr_test_ext_id = VmExtensionIdentifier(VmExtensionIds.GuestAgentDcrTestExtension.publisher, VmExtensionIds.GuestAgentDcrTestExtension.type, "1.1.5")
         dcr_test_ext = VmExtension(
             self._context.vm,
             dcr_test_ext_id,
-            resource_name="GuestAgentDcrTest")
+            resource_name="GuestAgentDcr-TestInstall")
+        dcr_ext = ExtensionInstall.GuestAgentDcrTestExtension(extension=dcr_test_ext)
 
         if is_arm64:
             log.info("Skipping test case for %s, since it has not been published on ARM64", VmExtensionIds.GuestAgentDcrTestExtension)
         else:
             log.info("Installing %s", dcr_test_ext)
+
+            dcr_ext.modify_ext_settings_and_enable()
+            dcr_ext.assert_instance_view()
 
             # setting_name = "%s-%s, %s: %s" % (dcr_test_ext_id.type, version, self.COUNT_KEY_NAME, self.enable_count)
             # dcr_test_ext.enable(
