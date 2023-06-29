d02c4fb735b0d1a3ec36abad8058c69c3833596c
nnandigam <nnandigam@microsoft.com>
2023-06-29 12:20:24 -0700

    address comments

--- azurelinuxagent/ga/agent_update_handler.py
+++ azurelinuxagent/ga/agent_update_handler.py
@@ -283,7 +283,7 @@ class AgentUpdateHandler(object):
     def run(self, goal_state):
         try:
             # Ignore new agents if update is disabled. The latter flag only used in e2e tests.
-            if not self._autoupdate_enabled or not conf.get_ga_updates_enabled():
+            if not self._autoupdate_enabled or not conf.get_download_new_agents():
                 return
 
             self._gs_id = goal_state.extensions_goal_state.id
