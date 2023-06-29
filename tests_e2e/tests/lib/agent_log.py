072bd5a188f989c45900c0b0860bfe9434b47284
nnandigam <nnandigam@microsoft.com>
2023-06-29 11:45:59 -0700

    remove

--- tests_e2e/tests/lib/agent_log.py
+++ tests_e2e/tests/lib/agent_log.py
@@ -92,11 +92,6 @@ class AgentLog(object):
             #
             # NOTE: This list was taken from the older agent tests and needs to be cleaned up. Feel free to un-comment rules as new tests are added.
             #
-            # # This warning is expected on SUSE 12
-            # {
-            #     'message': r"WARNING EnvHandler ExtHandler Move rules file 75-persistent-net-generator.rules to /var/lib/waagent/75-persistent-net-generator.rules",
-            #     'if': lambda _: re.match(r"((sles15\.2)|suse12)\D*", DISTRO_NAME, flags=re.IGNORECASE) is not None
-            # },
             # # The following message is expected to log an error if systemd is not enabled on it
             # {
             #     'message': r"Did not detect Systemd, unable to set wa(|linux)agent-network-setup.service",
@@ -139,6 +134,7 @@ class AgentLog(object):
             #     and r.prefix == "Daemon"
             # },
             #
+            # 2023-06-28T09:31:38.903835Z WARNING EnvHandler ExtHandler Move rules file 75-persistent-net-generator.rules to /var/lib/waagent/75-persistent-net-generator.rules
             #  The environment thread performs this operation periodically
             #
             {
