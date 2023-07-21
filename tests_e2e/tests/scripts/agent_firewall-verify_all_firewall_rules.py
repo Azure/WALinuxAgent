3498668ed4507db00481ddd251e2c553cd52286c
nnandigam <nnandigam@microsoft.com>
2023-07-21 12:48:39 -0700

    address comments

--- tests_e2e/tests/scripts/agent_firewall-verify_all_firewall_rules.py
+++ tests_e2e/tests/scripts/agent_firewall-verify_all_firewall_rules.py
@@ -19,7 +19,6 @@
 # This script checks all agent firewall rules added properly and working as expected
 #
 import argparse
-import logging
 import os
 import pwd
 import socket
@@ -27,7 +26,9 @@ import subprocess
 import sys
 import time
 import traceback
-from typing import List
+from typing import List, Tuple
+
+from tests_e2e.tests.lib.logging import log
 
 if sys.version_info[0] == 3:
     import http.client as httpclient
@@ -41,7 +42,7 @@ WIRESERVER_URL = '168.63.129.16'
 VERSIONS_PATH = '/?comp=versions'
 
 
-def get_wireserver_ip():
+def get_wireserver_ip() -> str:
     try:
         with open(WIRESERVER_ENDPOINT_FILE, 'r') as f:
             wireserver_ip = f.read()
@@ -50,30 +51,32 @@ def get_wireserver_ip():
     return wireserver_ip
 
 
-def switch_user(user):
-    # This function switches the function to a given user
+def switch_user(user: str) -> None:
+    """
+    This function switches the function to a given user
+    """
     try:
         uid = pwd.getpwnam(user)[2]
-        logging.info("uid:%s and user name:%s", uid, user)
+        log.info("uid:%s and user name:%s", uid, user)
         os.seteuid(uid)
     except Exception as e:
         raise Exception("Error -- failed to switch user to {0} : Failed with exception {1}".format(user, e))
 
 
-def get_root_accept_rule(command: str):
+def get_root_accept_rule(command: str) -> List[str]:
     return ['sudo', 'iptables', '-t', 'security', command, 'OUTPUT', '-d', get_wireserver_ip(), '-p', 'tcp', '-m',
             'owner',
             '--uid-owner',
             '0', '-j', 'ACCEPT', '-w']
 
 
-def get_non_root_accept_rule(command: str):
+def get_non_root_accept_rule(command: str) -> List[str]:
     return ['sudo', 'iptables', '-t', 'security', command, 'OUTPUT', '-d', get_wireserver_ip(), '-p', 'tcp',
             '--destination-port', '53', '-j',
             'ACCEPT', '-w']
 
 
-def get_non_root_drop_rule(command: str):
+def get_non_root_drop_rule(command: str) -> List[str]:
     return ['sudo', 'iptables', '-t', 'security', command, 'OUTPUT', '-d', get_wireserver_ip(), '-p', 'tcp', '-m',
             'conntrack', '--ctstate',
             'INVALID,NEW', '-j', 'DROP', '-w']
@@ -88,9 +91,11 @@ def execute_cmd(cmd: List[str]):
     return proc.returncode, stdout, stderr
 
 
-def check_if_iptable_rule_is_available(full_command: List[str]):
-    # This function is used to check if given rule is present in iptable rule set
-    # "-C" return exit code 0 if the rule is available.
+def check_if_iptable_rule_is_available(full_command: List[str]) -> bool:
+    """
+    This function is used to check if given rule is present in iptable rule set
+    "-C" return exit code 0 if the rule is available.
+    """
     exit_code, _, _ = execute_cmd(full_command)
     if exit_code == 0:
         return True
@@ -98,26 +103,30 @@ def check_if_iptable_rule_is_available(full_command: List[str]):
 
 
 def print_current_iptable_rules():
-    # This function prints the current iptable rules
+    """
+    This function prints the current iptable rules
+    """
     try:
         cmd = ['sudo', 'iptables', '-L', 'OUTPUT', '-t', 'security', '-nxv']
         exit_code, stdout, stderr = execute_cmd(cmd)
         if exit_code != 0:
-            logging.warning("Warning -- Failed to fetch the ip table rules with error code: %s and error: %s", exit_code, stderr)
+            log.warning("Warning -- Failed to fetch the ip table rules with error code: %s and error: %s", exit_code, stderr)
         else:
             for line in stdout.splitlines():
-                logging.info(str(line))
+                log.info(str(line))
     except Exception as error:
         raise Exception("Error -- Failed to fetch the ip table rule set {0}".format(error))
 
 
-def get_all_iptable_rule_commands(command: str):
+def get_all_iptable_rule_commands(command: str) -> Tuple[List[str], List[str], List[str]]:
     return get_root_accept_rule(command), get_non_root_accept_rule(command), get_non_root_drop_rule(command)
 
 
-def verify_all_rules_exist(max_retry: int = 5):
-    # This function is used to verify all the iptable rules are present in the rule set
-    logging.info("-----Verifying all ip table rules are present in rule set")
+def verify_all_rules_exist(max_retry: int = 5) -> None:
+    """
+    This function is used to verify all the iptable rules are present in the rule set
+    """
+    log.info("-----Verifying all ip table rules are present in rule set")
     retry: int = 0
     found: bool = False
     while retry < max_retry and not found:
@@ -126,7 +135,7 @@ def verify_all_rules_exist(max_retry: int = 5):
             non_root_accept) and check_if_iptable_rule_is_available(non_root_drop)
 
         if not found:
-            logging.info("Not all ip table rules are present, retrying in 30 secs.\n")
+            log.info("Not all ip table rules are present, retrying in 30 secs.\n")
             time.sleep(30)
         retry += 1
 
@@ -134,12 +143,14 @@ def verify_all_rules_exist(max_retry: int = 5):
         raise Exception("IP table rules missing in rule set.\n Current iptable rules:\n {0}".format(
             print_current_iptable_rules()))
 
-    logging.info("verified All ip table rules are present in rule set")
+    log.info("verified All ip table rules are present in rule set")
 
 
-def verify_rules_deleted_successfully(commands: List[List[str]] = None):
-    # This function is used to verify if provided rule or all(if not specified) iptable rules are deleted successfully.
-    logging.info("-----Verifying requested rules deleted successfully")
+def verify_rules_deleted_successfully(commands: List[List[str]] = None) -> None:
+    """
+    This function is used to verify if provided rule or all(if not specified) iptable rules are deleted successfully.
+    """
+    log.info("-----Verifying requested rules deleted successfully")
 
     if commands is None:
         commands = []
@@ -155,18 +166,20 @@ def verify_rules_deleted_successfully(commands: List[List[str]] = None):
         else:
             raise Exception("Deletion of ip table rules not successful\n.Current ip table rules:\n" + print_current_iptable_rules())
 
-    logging.info("ip table rules deleted successfully \n %s", commands)
+    log.info("ip table rules deleted successfully \n %s", commands)
 
 
-def delete_iptable_rules(commands: List[List[str]] = None):
-    # This function is used to delete the provided rule or all(if not specified) iptable rules
+def delete_iptable_rules(commands: List[List[str]] = None) -> None:
+    """
+    This function is used to delete the provided rule or all(if not specified) iptable rules
+    """
     if commands is None:
         commands = []
     if not commands:
         root_accept, non_root_accept, non_root_drop = get_all_iptable_rule_commands("-C")
         commands.extend([root_accept, non_root_accept, non_root_drop])
 
-    logging.info("-----Deleting ip table rules \n %s", commands)
+    log.info("-----Deleting ip table rules \n %s", commands)
 
     try:
         for command in commands:
@@ -174,12 +187,14 @@ def delete_iptable_rules(commands: List[List[str]] = None):
     except Exception as e:
         raise Exception("Error -- Failed to Delete the ip table rule set {0}".format(e))
 
-    logging.info("Success --Deletion of ip table rule")
+    log.info("Success --Deletion of ip table rule")
 
 
-def verify_dns_tcp_to_wireserver_is_allowed(user: str):
-    # This function is used to verify if tcp to wireserver is allowed for the given user
-    logging.info("-----Verifying DNS tcp to wireserver is allowed")
+def verify_dns_tcp_to_wireserver_is_allowed(user: str) -> None:
+    """
+    This function is used to verify if tcp to wireserver is allowed for the given user
+    """
+    log.info("-----Verifying DNS tcp to wireserver is allowed")
     switch_user(user)
     try:
         socket.create_connection((get_wireserver_ip(), 53), timeout=10)
@@ -188,23 +203,27 @@ def verify_dns_tcp_to_wireserver_is_allowed(user: str):
             "Error -- while using DNS TCP request as user:({0}), make sure the firewall rules are set correctly {1}".format(user,
                 e))
 
-    logging.info("Success -- can connect to wireserver port 53 using TCP as a user:(%s)", user)
+    log.info("Success -- can connect to wireserver port 53 using TCP as a user:(%s)", user)
 
 
-def verify_dns_tcp_to_wireserver_is_blocked(user: str):
-    # This function is used to verify if tcp to wireserver is blocked for given user
-    logging.info("-----Verifying DNS tcp to wireserver is blocked")
+def verify_dns_tcp_to_wireserver_is_blocked(user: str) -> None:
+    """
+    This function is used to verify if tcp to wireserver is blocked for given user
+    """
+    log.info("-----Verifying DNS tcp to wireserver is blocked")
     switch_user(user)
     try:
         socket.create_connection((get_wireserver_ip(), 53), timeout=10)
         raise Exception("Error -- unprivileged user:({0}) could connect to wireserver port 53 using TCP".format(user))
     except Exception:
-        logging.info("Success -- unprivileged user:(%s) access to wireserver port 53 using TCP is blocked", user)
+        log.info("Success -- unprivileged user:(%s) access to wireserver port 53 using TCP is blocked", user)
 
 
-def verify_http_to_wireserver_blocked(user: str):
-    # This function is used to verify if http to wireserver is blocked for the given user
-    logging.info("-----Verifying http request to wireserver is blocked")
+def verify_http_to_wireserver_blocked(user: str) -> None:
+    """
+     This function is used to verify if http to wireserver is blocked for the given user
+    """
+    log.info("-----Verifying http request to wireserver is blocked")
     switch_user(user)
     try:
         client = httpclient.HTTPConnection(get_wireserver_ip(), timeout=1)
@@ -221,12 +240,14 @@ def verify_http_to_wireserver_blocked(user: str):
     if not blocked:
         raise Exception("Error -- unprivileged user:({0}) could connect to wireserver, make sure the firewall rules are set correctly".format(user))
 
-    logging.info("Success -- unprivileged user:(%s) access to wireserver is blocked", user)
+    log.info("Success -- unprivileged user:(%s) access to wireserver is blocked", user)
 
 
-def verify_http_to_wireserver_allowed(user: str):
-    # This function is used to verify if http to wireserver is allowed for the given user
-    logging.info("-----Verifying http request to wireserver is allowed")
+def verify_http_to_wireserver_allowed(user: str) -> None:
+    """
+    This function is used to verify if http to wireserver is allowed for the given user
+    """
+    log.info("-----Verifying http request to wireserver is allowed")
     switch_user(user)
     try:
         client = httpclient.HTTPConnection(get_wireserver_ip(), timeout=1)
@@ -239,20 +260,22 @@ def verify_http_to_wireserver_allowed(user: str):
         # if we get exception, it means the request is failed to connect
         raise Exception("Error -- unprivileged user:({0}) access to wireserver failed:\n {1}".format(user, e))
 
-    logging.info("Success -- privileged user:(%s) access to wireserver is allowed", user)
+    log.info("Success -- privileged user:(%s) access to wireserver is allowed", user)
 
 
 def verify_non_root_accept_rule():
-    # This function verifies the non root accept rule and make sure it is re added by agent after deletion
-    logging.info("-----Verifying non root accept rule behavior")
-    logging.info("Before deleting the non root accept rule , ensure a non root user can do a tcp to wireserver but cannot do a http request")
+    """
+    This function verifies the non root accept rule and make sure it is re added by agent after deletion
+    """
+    log.info("-----Verifying non root accept rule behavior")
+    log.info("Before deleting the non root accept rule , ensure a non root user can do a tcp to wireserver but cannot do a http request")
     verify_dns_tcp_to_wireserver_is_allowed(NON_ROOT_USER)
     verify_http_to_wireserver_blocked(NON_ROOT_USER)
 
     # switch to root user required to stop the agent
     switch_user(ROOT_USER)
     # stop the agent, so that it won't re-add rules while checking
-    logging.info("Stop Guest Agent service")
+    log.info("Stop Guest Agent service")
     # agent-service is script name and stop is argument
     stop_agent = ["agent-service", "stop"]
     execute_cmd(stop_agent)
@@ -264,41 +287,43 @@ def verify_non_root_accept_rule():
     non_root_accept_check_cmd = get_non_root_accept_rule("-C")
     verify_rules_deleted_successfully([non_root_accept_check_cmd])
 
-    logging.info("** Current IP table rules\n")
+    log.info("** Current IP table rules\n")
     print_current_iptable_rules()
 
-    logging.info("After deleting the non root accept rule , ensure a non root user cannot do a tcp to wireserver request")
+    log.info("After deleting the non root accept rule , ensure a non root user cannot do a tcp to wireserver request")
     verify_dns_tcp_to_wireserver_is_blocked(NON_ROOT_USER)
 
     switch_user(ROOT_USER)
     # restart the agent to re-add the deleted rules
-    logging.info("Restart Guest Agent service")
+    log.info("Restart Guest Agent service")
     # agent-service is script name and start is argument
     start_agent = ["agent-service", "start"]
     execute_cmd(start_agent)
 
     verify_all_rules_exist()
-    logging.info("** Current IP table rules \n")
+    log.info("** Current IP table rules \n")
     print_current_iptable_rules()
 
-    logging.info("After appending the rule back , ensure a non root user can do a tcp to wireserver but cannot do a http request\n")
+    log.info("After appending the rule back , ensure a non root user can do a tcp to wireserver but cannot do a http request\n")
     verify_dns_tcp_to_wireserver_is_allowed(NON_ROOT_USER)
     verify_http_to_wireserver_blocked(NON_ROOT_USER)
 
-    logging.info("non root accept rule verified successfully\n")
+    log.info("non root accept rule verified successfully\n")
 
 
 def verify_root_accept_rule():
-    # This function verifies the root accept rule and make sure it is re added by agent after deletion
-    logging.info("-----Verifying root accept rule behavior")
-    logging.info("Before deleting the root accept rule , ensure a root user can do a http request but non root user cannot")
+    """
+    This function verifies the root accept rule and make sure it is re added by agent after deletion
+    """
+    log.info("-----Verifying root accept rule behavior")
+    log.info("Before deleting the root accept rule , ensure a root user can do a http request but non root user cannot")
     verify_http_to_wireserver_allowed(ROOT_USER)
     verify_http_to_wireserver_blocked(NON_ROOT_USER)
 
     # switch to root user required to stop the agent
     switch_user(ROOT_USER)
     # stop the agent, so that it won't re-add rules while checking
-    logging.info("Stop Guest Agent service")
+    log.info("Stop Guest Agent service")
     # agent-service is script name and stop is argument
     stop_agent = ["agent-service", "stop"]
     execute_cmd(stop_agent)
@@ -313,34 +338,36 @@ def verify_root_accept_rule():
     drop_check_cmd = get_non_root_drop_rule("-C")
     verify_rules_deleted_successfully([root_accept_check_cmd, drop_check_cmd])
 
-    logging.info("** Current IP table rules\n")
+    log.info("** Current IP table rules\n")
     print_current_iptable_rules()
 
     # restart the agent to re-add the deleted rules
-    logging.info("Restart Guest Agent service")
+    log.info("Restart Guest Agent service")
     # agent-service is script name and start is argument
     start_agent = ["agent-service", "start"]
     execute_cmd(start_agent)
 
     verify_all_rules_exist()
-    logging.info("** Current IP table rules \n")
+    log.info("** Current IP table rules \n")
     print_current_iptable_rules()
 
-    logging.info("After appending the rule back , ensure a root user can do a http request but non root user cannot")
+    log.info("After appending the rule back , ensure a root user can do a http request but non root user cannot")
     verify_dns_tcp_to_wireserver_is_allowed(NON_ROOT_USER)
     verify_http_to_wireserver_blocked(NON_ROOT_USER)
     verify_http_to_wireserver_allowed(ROOT_USER)
 
-    logging.info("root accept rule verified successfully\n")
+    log.info("root accept rule verified successfully\n")
 
 
 def verify_non_root_dcp_rule():
-    # This function verifies drop rule and make sure it is re added by agent after deletion
-    logging.info("-----Verifying non root drop rule behavior")
+    """
+    This function verifies drop rule and make sure it is re added by agent after deletion
+    """
+    log.info("-----Verifying non root drop rule behavior")
     # switch to root user required to stop the agent
     switch_user(ROOT_USER)
     # stop the agent, so that it won't re-add rules while checking
-    logging.info("Stop Guest Agent service")
+    log.info("Stop Guest Agent service")
     # agent-service is script name and stop is argument
     stop_agent = ["agent-service", "stop"]
     execute_cmd(stop_agent)
@@ -352,44 +379,42 @@ def verify_non_root_dcp_rule():
     non_root_drop_check_cmd = get_non_root_drop_rule("-C")
     verify_rules_deleted_successfully([non_root_drop_check_cmd])
 
-    logging.info("** Current IP table rules\n")
+    log.info("** Current IP table rules\n")
     print_current_iptable_rules()
 
-    logging.info("After deleting the non root drop rule, ensure a non root user can do http request to wireserver")
+    log.info("After deleting the non root drop rule, ensure a non root user can do http request to wireserver")
     verify_http_to_wireserver_allowed(NON_ROOT_USER)
 
     # restart the agent to re-add the deleted rules
-    logging.info("Restart Guest Agent service")
+    log.info("Restart Guest Agent service")
     # agent-service is script name and start is argument
     start_agent = ["agent-service", "start"]
     execute_cmd(start_agent)
 
     verify_all_rules_exist()
-    logging.info("** Current IP table rules\n")
+    log.info("** Current IP table rules\n")
     print_current_iptable_rules()
 
-    logging.info("After appending the rule back , ensure a non root user can do a tcp to wireserver but cannot do a http request")
+    log.info("After appending the rule back , ensure a non root user can do a tcp to wireserver but cannot do a http request")
     verify_dns_tcp_to_wireserver_is_allowed(NON_ROOT_USER)
     verify_http_to_wireserver_blocked(NON_ROOT_USER)
     verify_http_to_wireserver_allowed(ROOT_USER)
 
-    logging.info("non root drop rule verified successfully\n")
+    log.info("non root drop rule verified successfully\n")
 
 
 try:
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG, stream=sys.stdout)
 
-    # This user would be the non root user
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--user', required=True)
+    parser.add_argument('-u', '--user', required=True, help="Non root user")
     args = parser.parse_args()
 
     NON_ROOT_USER = args.user
 
-    verify_all_rules_exist()
-
-    logging.info("** Current IP table rules\n")
+    log.info("** Current IP table rules\n")
     print_current_iptable_rules()
+
+    verify_all_rules_exist()
 
     verify_non_root_accept_rule()
     verify_root_accept_rule()
@@ -398,5 +423,5 @@ try:
     sys.exit(0)
 
 except Exception as e:
-    logging.error("%s:\n%s", e, traceback.format_exc())
+    log.error("%s:\n%s", e, traceback.format_exc())
     sys.exit(1)
