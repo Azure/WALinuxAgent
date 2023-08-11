ebe68d4e217c49b830b15650cf5b297edfe78876
Maddie Ford <maddieford@microsoft.com>
2023-08-10 19:26:27 -0700

    Add multi-config test case

--- tests_e2e/tests/extensions_disabled/extensions_disabled.py
+++ tests_e2e/tests/extensions_disabled/extensions_disabled.py
@@ -41,6 +41,11 @@ from tests_e2e.tests.lib.virtual_machine_extension_client import VirtualMachineE
 
 
 class ExtensionsDisabled(AgentTest):
+    class TestCase:
+        def __init__(self, extension: VirtualMachineExtensionClient, settings: Any):
+            self.extension = extension
+            self.settings = settings
+
     def run(self):
         ssh_client: SshClient = self._context.create_ssh_client()
 
@@ -48,66 +53,57 @@ class ExtensionsDisabled(AgentTest):
         log.info("Disabling extension processing on the test VM [%s]", self._context.vm.name)
         output = ssh_client.run_command("update-waagent-conf Extensions.Enabled=n", use_sudo=True)
         log.info("Disable completed:\n%s", output)
-
         disabled_timestamp: datetime.datetime = datetime.datetime.utcnow() - datetime.timedelta(minutes=60)
 
-        #
-        # Validate that the agent is not processing extensions by attempting to run CSE & checking that provisioning fails fast
-        #
-        log.info("Executing CustomScript; the agent should report a VMExtensionProvisioningError without processing the extension")
-        custom_script = VirtualMachineExtensionClient(self._context.vm, VmExtensionIds.CustomScript, resource_name="CustomScript")
+        # Prepare test cases
+        unique = str(uuid.uuid4())
+        test_file = f"/tmp/waagent-test.{unique}"
+        test_cases = [
+            ExtensionsDisabled.TestCase(
+                VirtualMachineExtensionClient(self._context.vm, VmExtensionIds.CustomScript,
+                                              resource_name="CustomScript"),
+                {'commandToExecute': f"echo '{unique}' > {test_file}"}
+            ),
+            ExtensionsDisabled.TestCase(
+                VirtualMachineExtensionClient(self._context.vm, VmExtensionIds.RunCommandHandler,
+                                              resource_name="RunCommandHandler"),
+                {'source': {'script': f"echo '{unique}' > {test_file}"}}
+            )
+        ]
 
-        try:
-            # Use mkdir in script settings so we can check if the command was executed
-            custom_script.enable(settings={'commandToExecute': "mkdir /var/lib/waagent/testdir"}, force_update=True, timeout=6 * 60)
-            fail("The agent should have reported an error processing the goal state")
-        except Exception as error:
-            assert_that("VMExtensionProvisioningError" in str(error)) \
-                .described_as(f"Expected a VMExtensionProvisioningError error, but actual error was: {error}") \
-                .is_true()
-            assert_that("Extension will not be processed since extension processing is disabled" in str(error)) \
-                .described_as(f"Error message should communicate that extension will not be processed, but actual error "
-                              f"was: {error}").is_true()
-            log.info("Goal state processing failed as expected")
+        for t in test_cases:
+            log.info("Test case: %s", t.extension)
+            #
+            # Validate that the agent is not processing extensions by attempting to extension & checking that
+            # provisioning fails fast
+            #
+            log.info(
+                "Executing {0}; the agent should report a VMExtensionProvisioningError without processing the extension"
+                .format(t.extension.__str__()))
 
-        #
-        # Validate the agent is not processing extensions by checking it did not execute the extension settings
-        #
-        output = ssh_client.run_command("dir /var/lib/waagent", use_sudo=True)
-        assert_that(output) \
-            .described_as(
-            "Contents of '/var/lib/waagent' on test VM contains 'testdir': {output}. \n This indicates the extension was"
-            " unexpectedly processed") \
-            .does_not_contain('testdir')
-        log.info("The agent did not process the extension settings as expected")
+            try:
+                t.extension.enable(settings=t.settings, force_update=True, timeout=6 * 60)
+                fail("The agent should have reported an error processing the goal state")
+            except Exception as error:
+                assert_that("VMExtensionProvisioningError" in str(error)) \
+                    .described_as(f"Expected a VMExtensionProvisioningError error, but actual error was: {error}") \
+                    .is_true()
+                assert_that("Extension will not be processed since extension processing is disabled" in str(error)) \
+                    .described_as(
+                    f"Error message should communicate that extension will not be processed, but actual error "
+                    f"was: {error}").is_true()
+                log.info("Goal state processing for {0} failed as expected".format(t.extension.__str__()))
 
-        #
-        # Validate that the agent is not processing multi-config extensions
-        #
-        log.info(
-            "Executing RunCommand v2; the agent should report a VMExtensionProvisioningError without processing the extension")
-        run_command = VirtualMachineExtensionClient(self._context.vm, VmExtensionIds.RunCommandHandler,
-                                                    resource_name="RunCommandHandler")
-        try:
-            unique = str(uuid.uuid4())
-            test_file = f"/tmp/waagent-test.{unique}"
-            settings = \
-                {
-                    "source": {
-                        "script": f"echo '{unique}' > {test_file}"
-                    }
-                }
-
-            run_command.enable(settings=settings, force_update=True, timeout=6 * 60)
-            fail("The agent should have reported an error processing the goal state")
-        except Exception as error:
-            assert_that("VMExtensionProvisioningError" in str(error)) \
-                .described_as(f"Expected a VMExtensionProvisioningError error, but actual error was: {error}") \
-                .is_true()
-            assert_that("Extension will not be processed since extension processing is disabled" in str(error)) \
-                .described_as(f"Error message should communicate that extension will not be processed, but actual error "
-                              f"was: {error}").is_true()
-            log.info("Goal state processing failed as expected")
+            #
+            # Validate the agent did not process the extension by checking it did not execute the extension settings
+            #
+            output = ssh_client.run_command("dir /tmp", use_sudo=True)
+            assert_that(output) \
+                .described_as(
+                f"Contents of '/tmp' on test VM contains {test_file}: {output}. \n This indicates "
+                f"{t.extension.__str__()} was unexpectedly processed") \
+                .does_not_contain(f"{test_file}")
+            log.info("The agent did not process the extension settings as expected")
 
         #
         # Validate that the agent continued reporting status even if it is not processing extensions
@@ -130,10 +126,10 @@ class ExtensionsDisabled(AgentTest):
         output = ssh_client.run_command("update-waagent-conf Extensions.Enabled=y", use_sudo=True)
         log.info("Enable completed:\n%s", output)
 
-        try:
-            custom_script.enable(settings={'commandToExecute': "date"}, force_update=True, timeout=15 * 60)
-        except Exception as error:
-            log.info("Goal state processing unexpectedly failed after re-enabling extension processing")
+        for t in test_cases:
+            try:
+                t.extension.enable(settings=t.settings, force_update=True, timeout=15 * 60)
+                log.info("Goal state processing for {0} succeeded as expected".format(t.extension.__str__()))
 
 
     def get_ignore_error_rules(self) -> List[Dict[str, Any]]:
