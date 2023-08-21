f4725a96651d6b4f3c97c0b75fe76b5fc935fd7e
Maddie Ford <maddieford@microsoft.com>
2023-08-20 18:49:29 -0700

    Improve logging

--- tests_e2e/tests/ext_telemetry_pipeline/ext_telemetry_pipeline.py
+++ tests_e2e/tests/ext_telemetry_pipeline/ext_telemetry_pipeline.py
@@ -22,12 +22,13 @@
 # # validate if the events actually make it to wireserver
 #
 
-# from tests_e2e.tests.agent_bvt.vm_access import VmAccessBvt
+import random
+from typing import List, Dict, Any
+
 from tests_e2e.tests.lib.agent_test import AgentTest
 from tests_e2e.tests.lib.identifiers import VmExtensionIds
 from tests_e2e.tests.lib.logging import log
 from tests_e2e.tests.lib.ssh_client import SshClient
-from tests_e2e.tests.lib.virtual_machine_client import VirtualMachineClient
 from tests_e2e.tests.lib.virtual_machine_extension_client import VirtualMachineExtensionClient
 
 
@@ -41,31 +42,50 @@ class ExtTelemetryPipeline(AgentTest):
         output = ssh_client.run_command("update-waagent-conf Debug.EtpCollectionPeriod=30", use_sudo=True)
         log.info("Updated waagent conf with Debug.ETPCollectionPeriod=30 completed:\n%s", output)
 
-        # Add VmAccess to the test VM
-        log.info("")
-        log.info("Add VmAccess ext to the test VM and ensure vm is accessible with the new credentials...")
-        # TODO VmAccessBvt.run()
-
-        # Add CSE to the test VM
+        # Add CSE to the test VM twice to ensure its events directory still exists after re-enabling
         log.info("")
         log.info("Add CSE to the test VM...")
         cse = VirtualMachineExtensionClient(self._context.vm, VmExtensionIds.CustomScript, resource_name="CustomScript")
-        cse.enable(settings={'commandToExecute': "date"})
+        cse.enable(settings={'commandToExecute': "echo 'enable'"})
+        cse.assert_instance_view()
+
+        log.info("")
+        log.info("Add CSE to the test VM again...")
+        cse.enable(settings={'commandToExecute': "echo 'enable again'"})
         cse.assert_instance_view()
 
         # Check agent log to verify ETP is enabled
-        command = f"agent_ext_workflow-check_data_in_agent_log.py --data 'Extension Telemetry pipeline enabled: True'"
+        command = "agent_ext_workflow-check_data_in_agent_log.py --data 'Extension Telemetry pipeline enabled: True'"
         log.info("")
-        log.info("Check agent log to verify ETP is enabled...".format(command))
+        log.info("Check agent log to verify ETP is enabled...")
         log.info("Remote command [%s] completed:\n%s", command, ssh_client.run_command(command))
 
-        # TODO: Validate there is an events dir for each extension on the machine
+        extensions = ["Microsoft.OSTCExtensions.VMAccessForLinux", "Microsoft.Azure.Extensions.CustomScript"]
 
-        # TODO: Ensure good events are reported
+        log.info("")
+        log.info("Add good extension events and check they are reported...")
+        max_events = random.randint(10, 50)
+        output = ssh_client.run_command(f"ext_telemetry_pipeline-add_extension_events.py "
+                                        f"--extensions {','.join(extensions)} "
+                                        f"--num_events_total {max_events}", use_sudo=True)
+        log.info(output)
+        log.info("")
+        log.info("Good extension events were successfully reported.")
 
-        # TODO: Ensure bad events are reported
+        log.info("")
+        log.info("Add bad extension events and check they are reported...")
+        output = ssh_client.run_command(f"ext_telemetry_pipeline-add_extension_events.py "
+                                        f"--extensions {','.join(extensions)} "
+                                        f"--num_events_total {max_events} "
+                                        f"--num_events_bad {random.randint(5, max_events-5)}", use_sudo=True)
+        log.info(output)
+        log.info("")
+        log.info("Bad extension events were successfully dropped.")
 
-        # TODO: Ensure all events are deleted
+    def get_ignore_error_rules(self) -> List[Dict[str, Any]]:
+        return [
+            {'message': r"Dropped events for Extension.*"}
+        ]
 
 
 if __name__ == "__main__":
