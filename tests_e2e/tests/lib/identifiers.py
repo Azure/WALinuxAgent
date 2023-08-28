e86f1a5556341e376deb94d2b9389988beca259f
maddieford <93676569+maddieford@users.noreply.github.com>
2023-08-28 15:37:14 -0700

    extension telemetry pipeline scenario (#2901)
    * Update version to dummy 1.0.0.0'
    
    * Revert version change
    
    * Barebones for etp
    
    * Scenario should own VM because of conf change
    
    * Add extension telemetry pipeline test
    
    * Clean up code
    
    * Improve log messages
    
    * Fix pylint errors
    
    * Improve logging
    
    * Improve code comments
    
    * VmAccess is not supported on flatcar
    
    * Address PR comments
    
    * Add support_distros in VmExtensionIdentifier
    
    * Fix logic for support_distros in VmExtensionIdentifier
    
    * Use run_remote_test for remote script

--- tests_e2e/tests/lib/identifiers.py
+++ tests_e2e/tests/lib/identifiers.py
@@ -15,6 +15,8 @@
 # limitations under the License.
 #
 
+from typing import Dict, List
+
 
 class VmIdentifier(object):
     def __init__(self, cloud: str, location: str, subscription: str, resource_group: str, name: str):
@@ -44,6 +46,19 @@ class VmExtensionIdentifier(object):
         self.publisher: str = publisher
         self.type: str = ext_type
         self.version: str = version
+
+    unsupported_distros: Dict[str, List[str]] = {
+        "Microsoft.OSTCExtensions.VMAccessForLinux": ["flatcar"]
+    }
+
+    def supports_distro(self, system_info: str) -> bool:
+        """
+        Returns true if an unsupported distro name for the extension is found in the provided system info
+        """
+        ext_unsupported_distros = VmExtensionIdentifier.unsupported_distros.get(self.publisher + "." + self.type)
+        if ext_unsupported_distros is not None and any(distro in system_info for distro in ext_unsupported_distros):
+            return False
+        return True
 
     def __str__(self):
         return f"{self.publisher}.{self.type}"
