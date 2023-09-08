087e9264163c964dde73635567840013cbc7356b
Nageswara Nandigam <84482346+nagworld9@users.noreply.github.com>
2023-09-07 17:08:17 -0700

    disable ga versioning changes (#2917)
    * Disable ga versioning changes (#2909)
    
    * disbale rsm changes
    
    * add flag
    
    (cherry picked from commit 5a4fae833a92de4d44b1939e48678a043132fbd4)
    
    * merge conflicts

--- tests_e2e/tests/agent_update/rsm_update.py
+++ tests_e2e/tests/agent_update/rsm_update.py
@@ -139,7 +139,7 @@ class RsmUpdateBvt(AgentTest):
         log.info('Successfully updated agent installed version')
         if update_config:
             log.info('Executing update-waagent-conf remote script to update agent update config flags to allow and download test versions')
-            self._run_remote_test("update-waagent-conf Debug.DownloadNewAgents=y AutoUpdate.GAFamily=Test", use_sudo=True)
+            self._run_remote_test("update-waagent-conf Debug.EnableGAVersioning=y AutoUpdate.GAFamily=Test", use_sudo=True)
             log.info('Successfully updated agent update config')
 
     @staticmethod
