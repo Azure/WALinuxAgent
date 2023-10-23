af04f93db716fbdcc4b563244f3fcbbaf731a1b6
nnandigam <nnandigam@microsoft.com>
2023-10-23 13:27:26 -0700

    addressed comment

--- azurelinuxagent/ga/agent_update_handler.py
+++ azurelinuxagent/ga/agent_update_handler.py
@@ -305,7 +305,7 @@ class AgentUpdateHandler(object):
                 if not self.__should_agent_attempt_manifest_download():
                     return
                 if conf.get_enable_ga_versioning():  # log the warning only when ga versioning is enabled
-                    # Need to revisit this msg when version is missing in Goal state. We may need to handle better way to report the error
+                    # TODO: Need to revisit this msg when version is missing in Goal state. We may need to handle better way to report the error
                     warn_msg = "Missing requested version in agent family: {0} for incarnation: {1}, fallback to largest version update".format(self._ga_family, self._gs_id)
                     GAUpdateReportState.report_error_msg = warn_msg
                 agent_manifest = goal_state.fetch_agent_manifest(agent_family.name, agent_family.uris)
@@ -342,8 +342,9 @@ class AgentUpdateHandler(object):
                 raise AgentUpdateError("The Agent received a request to downgrade to version {0}, but downgrading to a version less than "
                                        "the Agent installed on the image ({1}) is not supported. Skipping downgrade.".format(requested_version, daemon_version))
 
-            msg = "Goal state {0} is requesting a new agent version {1} [IsRSMUpdate:{2}], will update the agent before processing the goal state.".format(
-                self._gs_id, str(requested_version), self._is_requested_version_update)
+            # Todo: Need to update the message when we fix RSM stuff
+            msg = "Self-update discovered new agent version:{0} in agent manifest for goal state {1}, will update the agent before processing the goal state.".format(
+                str(requested_version), self._gs_id)
             self.__log_event(LogLevel.INFO, msg)
 
             agent = self.__download_and_get_agent(goal_state, agent_family, agent_manifest, requested_version)
