5b04dc8db2b8fe2b56dd56fe265072d0c937508b
Nageswara Nandigam <84482346+nagworld9@users.noreply.github.com>
2023-10-31 11:58:49 -0700

    Reduce the firewall check period in agent firewall tests (#2966)
    * reduce firewall check period
    
    * reduce firewall check period

--- tests_e2e/tests/scripts/agent_firewall-verify_all_firewall_rules.py
+++ tests_e2e/tests/scripts/agent_firewall-verify_all_firewall_rules.py
@@ -39,7 +39,7 @@ ROOT_USER = 'root'
 WIRESERVER_ENDPOINT_FILE = '/var/lib/waagent/WireServerEndpoint'
 WIRESERVER_IP = '168.63.129.16'
 VERSIONS_PATH = '/?comp=versions'
-FIREWALL_PERIOD = 60
+FIREWALL_PERIOD = 30
 
 
 class FirewallRules(object):
@@ -144,7 +144,7 @@ def verify_all_rules_exist() -> None:
 
     log.info("-----Verifying all ip table rules are present in rule set")
     # Agent will re-add rules within OS.EnableFirewallPeriod, So waiting that time + some buffer
-    found: bool = retry_if_false(check_all_iptables, attempts=2, delay=FIREWALL_PERIOD+30)
+    found: bool = retry_if_false(check_all_iptables, attempts=2, delay=FIREWALL_PERIOD+15)
 
     if not found:
         fail("IP table rules missing in rule set.\n Current iptable rules:\n {0}".format(
