36c4c77f0e29e43cb3358bcd9e175305867d5c63
Maddie Ford <maddieford@microsoft.com>
2024-01-22 12:53:01 -0800

    e2e test for recovering the network interface on redhat distros

--- tests_e2e/tests/recover_network_interface/recover_network_interface.py
+++ tests_e2e/tests/recover_network_interface/recover_network_interface.py
@@ -23,19 +23,19 @@
 # affects this logic.
 #
 
-import datetime
-import re
+import json
 
-from assertpy import fail
+from assertpy import fail, assert_that
 from time import sleep
 
-from tests_e2e.tests.lib.shell import CommandError
-from tests_e2e.tests.lib.agent_test import AgentVmTest, TestSkipped
+from tests_e2e.tests.lib.agent_test import AgentVmTest
 from tests_e2e.tests.lib.agent_test_context import AgentVmTestContext
 from tests_e2e.tests.lib.logging import log
+from tests_e2e.tests.lib.virtual_machine_extension_client import VirtualMachineExtensionClient
+from tests_e2e.tests.lib.vm_extension_identifier import VmExtensionIds
 
 
-class PublishHostname(AgentVmTest):
+class RecoverNetworkInterface(AgentVmTest):
     def __init__(self, context: AgentVmTestContext):
         super().__init__(context)
         self._context = context
@@ -69,113 +69,56 @@ class PublishHostname(AgentVmTest):
 
         if not status_updated:
             fail("Agent hasn't reported status since {0} and ssh connection failed. Use the serial console in portal "
-                 "to check the contents of '/sys/class/net/eth0/operstate'. If the contents of this file are 'up', "
-                 "no further action is needed. If contents are 'down', that indicates the network interface is down "
-                 "and more debugging needs to be done to confirm this is not caused by the agent.\n VM: {1}\n RG: {2}"
-                 "\nSubscriptionId: {3}\nUsername: {4}\nPassword: {5}".format(last_agent_status_time,
-                                                                              self._context.vm,
-                                                                              self._context.vm.resource_group,
-                                                                              self._context.vm.subscription,
-                                                                              self._context.username,
-                                                                              self._vm_password))
-
-    def retry_ssh_if_connection_reset(self, command: str, use_sudo=False):
-        # The agent may bring the network down and back up to publish the hostname, which can reset the ssh connection.
-        # Adding retry here for connection reset.
-        retries = 3
-        while retries > 0:
-            try:
-                return self._ssh_client.run_command(command, use_sudo=use_sudo)
-            except CommandError as e:
-                retries -= 1
-                retryable = e.exit_code == 255 and "Connection reset by peer" in e.stderr
-                if not retryable or retries == 0:
-                    raise
-                log.warning("The SSH operation failed, retrying in 30 secs")
-                sleep(30)
+                 "to debug".format(last_agent_status_time))
 
     def run(self):
-        # TODO: Investigate why hostname is not being published on Ubuntu as expected
-        if "ubuntu" in self._ssh_client.run_command("get_distro.py").lower():
-            raise TestSkipped("Known issue with hostname publishing on ubuntu. Will skip test until we continue "
-                              "investigation.")
-
         # Add password to VM and log. This allows us to debug with serial console if necessary
+        log.info("")
+        log.info("Adding password to the VM to use for debugging in case necessary...")
         self.add_vm_password()
 
-        # This test looks up what hostname is published to dns. Check that the tools necessary to get hostname are
-        # installed, and if not install them.
-        lookup_cmd, dns_regex = self.check_and_install_dns_tools()
+        # Get the primary network interface name
+        ifname = self._ssh_client.run_command("pypy3 -c 'from azurelinuxagent.common.osutil.redhat import RedhatOSUtil; print(RedhatOSUtil().get_if_name())'").rstrip()
+        # The interface name needs to be in double quotes for the pypy portion of the script
+        formatted_ifname = f'"{ifname}"'
 
-        # Check if this distro monitors hostname changes. If it does, we should check that the agent detects the change
-        # and publishes the host name. If it doesn't, we should check that the hostname is automatically published.
-        monitors_hostname = self._ssh_client.run_command("get-waagent-conf-value Provisioning.MonitorHostName", use_sudo=True).rstrip().lower()
+        # The script should bring the primary network interface down and use the agent to recover the interface. These
+        # commands will bring the network down, so they should be executed on the machine using CSE instead of ssh.
+        script = f"""
+        ifdown {ifname};
+        nic_state=$(nmcli -g general.state device show {ifname})
+        echo Primary network interface state before recovering: $nic_state
+        source /home/{self._context.username}/bin/set-agent-env;
+        pypy3 -c 'from azurelinuxagent.common.osutil.redhat import RedhatOSUtil; RedhatOSUtil().check_and_recover_nic_state({formatted_ifname})';
+        nic_state=$(nmcli -g general.state device show {ifname});
+        echo Primary network interface state after recovering: $nic_state
+        """
+        log.info("")
+        log.info("Using CSE to bring the primary network interface down and call the OSUtil to bring the interface back up. Command to execute: {0}".format(script))
+        custom_script = VirtualMachineExtensionClient(self._context.vm, VmExtensionIds.CustomScript, resource_name="CustomScript")
+        custom_script.enable(
+            protected_settings={
+                'commandToExecute': script
+            }
+        )
 
-        hostname_change_ctr = 0
-        # Update the hostname 3 times
-        while hostname_change_ctr < 3:
-            try:
-                hostname = "hostname-monitor-{0}".format(hostname_change_ctr)
-                log.info("Update hostname to {0}".format(hostname))
-                self.retry_ssh_if_connection_reset("hostnamectl set-hostname {0}".format(hostname), use_sudo=True)
+        # Check that the interface was down and brought back up in instance view
+        log.info("")
+        log.info("Checking the instance view to confirm the primary network interface was brought down and successfully recovered by the agent...")
+        instance_view = custom_script.get_instance_view()
+        log.info("Instance view for custom script after enable is: {0}".format(json.dumps(instance_view.serialize(), indent=4)))
+        assert_that(len(instance_view.statuses)).described_as("Instance view should have a status for CustomScript").is_greater_than(0)
+        assert_that(instance_view.statuses[0].message).described_as("The primary network interface should be in a disconnected state before the attempt to recover").contains("Primary network interface state before recovering: 30 (disconnected)")
+        assert_that(instance_view.statuses[0].message).described_as("The primary network interface should be in a connected state after the attempt to recover").contains("Primary network interface state after recovering: 100 (connected)")
 
-                # Wait for the agent to detect the hostname change for up to 2 minutes if hostname monitoring is enabled
-                if monitors_hostname == "y" or monitors_hostname == "yes":
-                    log.info("Agent hostname monitoring is enabled")
-                    timeout = datetime.datetime.now() + datetime.timedelta(minutes=2)
-                    hostname_detected = ""
-                    while datetime.datetime.now() <= timeout:
-                        try:
-                            hostname_detected = self.retry_ssh_if_connection_reset("grep -n 'Detected hostname change:.*-> {0}' /var/log/waagent.log".format(hostname), use_sudo=True)
-                            if hostname_detected:
-                                log.info("Agent detected hostname change: {0}".format(hostname_detected))
-                                break
-                        except CommandError as e:
-                            # Exit code 1 indicates grep did not find a match. Sleep if exit code is 1, otherwise raise.
-                            if e.exit_code != 1:
-                                raise
-                        sleep(15)
+        # Check that the agent is successfully reporting status after recovering the network
+        log.info("")
+        log.info("Checking that the agent is reporting status after recovering the network...")
+        self.check_agent_reports_status()
 
-                    if not hostname_detected:
-                        fail("Agent did not detect hostname change: {0}".format(hostname))
-                else:
-                    log.info("Agent hostname monitoring is disabled")
-
-                # Check that the expected hostname is published with 4 minute timeout
-                timeout = datetime.datetime.now() + datetime.timedelta(minutes=4)
-                published_hostname = ""
-                while datetime.datetime.now() <= timeout:
-                    try:
-                        dns_info = self.retry_ssh_if_connection_reset(lookup_cmd)
-                        actual_hostname = re.match(dns_regex, dns_info)
-                        if actual_hostname:
-                            # Compare published hostname to expected hostname
-                            published_hostname = actual_hostname.group('hostname')
-                            if hostname == published_hostname:
-                                log.info("SUCCESS Hostname {0} was published successfully".format(hostname))
-                                break
-                        else:
-                            log.info("Unable to parse the dns info: {0}".format(dns_info))
-                    except CommandError as e:
-                        if "NXDOMAIN" in e.stdout:
-                            log.info("DNS Lookup could not find domain. Will try again.")
-                        else:
-                            raise
-                    sleep(30)
-
-                if published_hostname == "" or published_hostname != hostname:
-                    fail("Hostname {0} was not published successfully. Actual host name is: {1}".format(hostname, published_hostname))
-
-                hostname_change_ctr += 1
-
-            except CommandError as e:
-                # If failure is ssh issue, we should confirm that the VM did not lose network connectivity due to the
-                # agent's operations on the network. If agent reports status after this failure, then we know the
-                # network is up.
-                if e.exit_code == 255 and ("Connection timed out" in e.stderr or "Connection refused" in e.stderr):
-                    self.check_agent_reports_status()
-                raise
+        log.info("")
+        log.info("The primary network was successfully recovered by the agent.")
 
 
 if __name__ == "__main__":
-    PublishHostname.run_from_command_line()
+    RecoverNetworkInterface.run_from_command_line()
