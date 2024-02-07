e97d63b7232728ec0f82bb2571949d4dbb359979
Norberto Arrieta <narrieta@users.noreply.github.com>
2024-02-07 15:42:52 -0800

    Check for 'Access denied' errors when testing SSH connectivity (#3042)
    Co-authored-by: narrieta <narrieta>

--- tests_e2e/orchestrator/lib/agent_test_suite.py
+++ tests_e2e/orchestrator/lib/agent_test_suite.py
@@ -491,7 +491,7 @@ class AgentTestSuite(LisaTestSuite):
                 break
             except CommandError as error:
                 # Check for "System is booting up. Unprivileged users are not permitted to log in yet. Please come back later. For technical details, see pam_nologin(8)."
-                if "Unprivileged users are not permitted to log in yet" not in error.stderr:
+                if not any(m in error.stderr for m in ["Unprivileged users are not permitted to log in yet", "Permission denied"]):
                     raise
                 if attempt >= max_attempts - 1:
                     raise Exception(f"SSH connectivity check failed after {max_attempts} attempts, giving up [{error}]")
