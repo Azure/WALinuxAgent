59d646f3f2a3b255540f19643415c4d7fd177be9
Nageswara Nandigam <nnandigam@microsoft.com>
2024-09-12 16:58:06 -0700

    unicode type check

--- azurelinuxagent/ga/exthandlers.py
+++ azurelinuxagent/ga/exthandlers.py
@@ -2305,9 +2305,11 @@ class HandlerManifest(object):
         """
         Expects boolean value but
         for backward compatibility, 'true' (case-insensitive) is accepted, and other values default to False
+        Note: In py2, string can be unicode or str type. In py3, unicode removed
+        ustr is a unicode object for Py2 and a str object for Py3.
         """
         if not isinstance(value, bool):
-            return True if isinstance(value, str) and value.lower() == "true" else default_val
+            return True if isinstance(value, ustr) or isinstance(value, str) and value.lower() == "true" else default_val
         return value
 
 
