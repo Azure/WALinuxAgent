ffd60645bc55bbd8be79fb5a16d417cff2278743
narrieta@microsoft <narrieta>
2025-01-10 11:50:28 -0800

    Use Ubuntu 24 for unit tests runs [Python 2.6 & 3.4]

--- azurelinuxagent/common/utils/textutil.py
+++ azurelinuxagent/common/utils/textutil.py
@@ -17,7 +17,6 @@
 # Requires Python 2.6+ and Openssl 1.0+
 
 import base64
-import hashlib
 import re
 import struct
 import sys
@@ -383,19 +382,6 @@ def is_str_none_or_whitespace(s):
 
 def is_str_empty(s):
     return is_str_none_or_whitespace(s) or is_str_none_or_whitespace(s.rstrip(' \t\r\n\0'))
-
-
-def hash_strings(string_list):
-    """
-    Compute a cryptographic hash of a list of strings
-
-    :param string_list: The strings to be hashed
-    :return: The cryptographic hash (digest) of the strings in the order provided
-    """
-    sha1_hash = hashlib.sha1()
-    for item in string_list:
-        sha1_hash.update(item.encode())
-    return sha1_hash.digest()
 
 
 def format_memory_value(unit, value):
