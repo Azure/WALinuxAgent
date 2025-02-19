1080ad05159dcc365ef27aa44319c6aa49b3051b
Nageswara Nandigam <84482346+nagworld9@users.noreply.github.com>
2025-02-19 11:21:47 -0800

    Enable resource monitoring in cgroup v2 machines (#3316)
    * enable resource monitoring in v2
    
    * address comments
    
    * address comment
    
    * new comments
    
    * ut fix

--- azurelinuxagent/ga/cgroupconfigurator.py
+++ azurelinuxagent/ga/cgroupconfigurator.py
@@ -185,8 +185,9 @@ class CGroupConfigurator(object):
                 for controller in agent_controllers:
                     for prop in controller.get_unit_properties():
                         log_cgroup_info('Agent {0} unit property value: {1}'.format(prop, systemd.get_unit_property(systemd.get_agent_unit_name(), prop)))
-                    if isinstance(controller, _CpuController):
+                    if isinstance(controller, _CpuController) and self._cgroups_api.can_enforce_cpu():
                         self._set_cpu_quota(agent_unit_name, conf.get_agent_cpu_quota())
+                        controller.track_throttle_time(True)  # CPU controller track the throttle time only when CPU quota is set
                     elif isinstance(controller, _MemoryController):
                         self._agent_memory_metrics = controller
                     CGroupsTelemetry.track_cgroup_controller(controller)
@@ -230,8 +231,7 @@ class CGroupConfigurator(object):
                 return False
 
             if self.using_cgroup_v2():
-                log_cgroup_info("Agent and extensions resource enforcement and monitoring is not currently supported on cgroup v2", send_event=True)
-                return False
+                log_cgroup_info("Only resource monitoring is currently supported on cgroup v2", send_event=True)
 
             return True
 
@@ -407,41 +407,6 @@ class CGroupConfigurator(object):
                     CGroupConfigurator._Impl._cleanup_unit_file(unit_file)
                 return
 
-        @staticmethod
-        def _get_current_cpu_quota(unit_name):
-            """
-            Calculate the CPU percentage from CPUQuotaPerSecUSec for given unit.
-            Params:
-                cpu_quota_per_sec_usec (str): The value of CPUQuotaPerSecUSec (e.g., "1s", "500ms", "500us", or "infinity").
-
-            Returns:
-                str: CPU percentage, or 'infinity' or 'unknown' if we can't determine the value.
-            """
-            try:
-                cpu_quota_per_sec_usec = systemd.get_unit_property(unit_name, "CPUQuotaPerSecUSec").strip().lower()
-                if cpu_quota_per_sec_usec == "infinity":
-                    return cpu_quota_per_sec_usec  # No limit on CPU usage
-
-                # Parse the value based on the suffix
-                elif cpu_quota_per_sec_usec.endswith("us"):
-                    # Directly use the microseconds value
-                    cpu_quota_us = float(cpu_quota_per_sec_usec[:-2])
-                elif cpu_quota_per_sec_usec.endswith("ms"):
-                    # Convert milliseconds to microseconds
-                    cpu_quota_us = float(cpu_quota_per_sec_usec[:-2]) * 1000
-                elif cpu_quota_per_sec_usec.endswith("s"):
-                    # Convert seconds to microseconds
-                    cpu_quota_us = float(cpu_quota_per_sec_usec[:-1]) * 1000000
-                else:
-                    raise ValueError("Invalid format. Expected 's', 'ms', 'us', or 'infinity'.")
-
-                # Calculate CPU percentage
-                cpu_percentage = (cpu_quota_us / 1000000) * 100
-                return "{0:g}%".format(cpu_percentage)  # :g Removes trailing zeros after decimal point
-            except Exception as e:
-                log_cgroup_warning("Error parsing current CPUQuotaPerSecUSec: {0}".format(ustr(e)))
-                return "unknown"
-
         def supported(self):
             return self._cgroups_supported
 
@@ -515,7 +480,7 @@ class CGroupConfigurator(object):
         @staticmethod
         def _try_set_cpu_quota(unit_name, quota):  # pylint: disable=unused-private-member
             try:
-                current_cpu_quota = CGroupConfigurator._Impl._get_current_cpu_quota(unit_name)
+                current_cpu_quota = CGroupUtil.get_current_cpu_quota(unit_name)
                 if current_cpu_quota == quota:
                     return
                 quota = quota if quota != "infinity" else ""  # no-quota expressed as empty string while setting property
@@ -786,7 +751,10 @@ class CGroupConfigurator(object):
                     cgroup = self._cgroups_api.get_unit_cgroup(unit_name, unit_name)
                     controllers = cgroup.get_controllers()
 
+                    has_cpu_quota = CGroupUtil.has_cpu_quota(unit_name)
                     for controller in controllers:
+                        if isinstance(controller, _CpuController) and has_cpu_quota:
+                            controller.track_throttle_time(True) # CPU controller track the throttle time only when CPU quota is set
                         CGroupsTelemetry.track_cgroup_controller(controller)
 
                 except Exception as exception:
@@ -864,7 +832,7 @@ class CGroupConfigurator(object):
             if memory_accounting != "yes":
                 properties_to_update += ("MemoryAccounting",)
                 properties_values += ("yes",)
-            current_cpu_quota = CGroupConfigurator._Impl._get_current_cpu_quota(unit_name)
+            current_cpu_quota = CGroupUtil.get_current_cpu_quota(unit_name)
             if current_cpu_quota != cpu_quota:
                 properties_to_update += ("CPUQuota",)
                 # no-quota expressed as empty string while setting property
@@ -896,7 +864,7 @@ class CGroupConfigurator(object):
                         CGroupConfigurator._Impl._cleanup_unit_file(old_extension_slice_path)
 
                     cpu_quota = "{0}%".format(
-                        cpu_quota) if cpu_quota is not None else "infinity"  # following systemd convention for no-quota (infinity)
+                        cpu_quota) if cpu_quota is not None and self._cgroups_api.can_enforce_cpu() else "infinity"  # following systemd convention for no-quota (infinity)
                     properties_to_update, properties_values = self._get_unit_properties_requiring_update(extension_slice, cpu_quota)
 
                     if len(properties_to_update) > 0:
@@ -949,7 +917,7 @@ class CGroupConfigurator(object):
                         self._cleanup_all_files(files_to_remove)
 
                         cpu_quota = service.get('cpuQuotaPercentage')
-                        cpu_quota = "{0}%".format(cpu_quota) if cpu_quota is not None else "infinity"  # following systemd convention for no-quota (infinity)
+                        cpu_quota = "{0}%".format(cpu_quota) if cpu_quota is not None and self._cgroups_api.can_enforce_cpu() else "infinity"  # following systemd convention for no-quota (infinity)
                         properties_to_update, properties_values = self._get_unit_properties_requiring_update(service_name, cpu_quota)
                         # If systemd is unaware of extension services and not loaded in the system yet, we get error while setting quotas. Hence, added unit loaded check.
                         if systemd.is_unit_loaded(service_name) and len(properties_to_update) > 0:
--- tests/ga/test_cgroupconfigurator.py
+++ tests/ga/test_cgroupconfigurator.py
@@ -91,10 +91,6 @@ class CGroupConfiguratorSystemdTestCase(AgentTestCase):
         with self._get_cgroup_configurator() as configurator:
             self.assertTrue(configurator.enabled(), "cgroups were not enabled")
 
-    def test_initialize_should_not_enable_cgroups_v2(self):
-        with self._get_cgroup_configurator_v2() as configurator:
-            self.assertFalse(configurator.enabled(), "cgroups were enabled")
-
     def test_initialize_should_not_enable_when_cgroup_api_cannot_be_determined(self):
         # Mock cgroup api to raise CGroupsException
         def mock_create_cgroup_api():
@@ -175,6 +171,43 @@ class CGroupConfiguratorSystemdTestCase(AgentTestCase):
             self.assertFalse(configurator.enabled(), "Cgroups should not be enabled")
             self.assertEqual(len(tracked), 0, "No cgroups should be tracked. Tracked: {0}".format(tracked))
             self.assertFalse(os.path.exists(agent_drop_in_file_cpu_quota), "{0} should not have been created".format(agent_drop_in_file_cpu_quota))
+
+    def test_initialize_should_enable_cgroups_v2(self):
+        with self._get_cgroup_configurator_v2() as configurator:
+            self.assertTrue(configurator.enabled(), "cgroups were not enabled")
+
+    def test_initialize_should_start_tracking_the_agent_cgroups_in_v2(self):
+        with self._get_cgroup_configurator_v2() as configurator:
+            tracked = CGroupsTelemetry._tracked
+
+            self.assertTrue(configurator.enabled(), "Cgroups should be enabled")
+            self.assertTrue(any(cg for cg in tracked if tracked[cg].name == AGENT_NAME_TELEMETRY and 'cpu' in cg),
+                "The Agent's CPU is not being tracked. Tracked: {0}".format(tracked))
+            self.assertTrue(any(cg for cg in tracked if tracked[cg].name == AGENT_NAME_TELEMETRY and 'memory' in cg),
+                "The Agent's Memory is not being tracked. Tracked: {0}".format(tracked))
+
+    def test_initialize_should_not_enable_cgroups_when_the_cpu_and_memory_controllers_are_not_present_in_v2(self):
+        with patch('azurelinuxagent.ga.cgroupapi.SystemdCgroupApiv2._get_controllers_enabled_at_root', return_value=[]):
+            with self._get_cgroup_configurator_v2() as configurator:
+                tracked = CGroupsTelemetry._tracked
+
+                self.assertFalse(configurator.enabled(), "Cgroups should not be enabled")
+                self.assertEqual(len(tracked), 0, "No cgroups should be tracked. Tracked: {0}".format(tracked))
+
+    def test_initialize_should_start_tracking_other_controllers_when_one_is_not_present_in_v2(self):
+        with patch('azurelinuxagent.ga.cgroupapi.SystemdCgroupApiv2._get_controllers_enabled_at_root', return_value=['memory']):
+            with self._get_cgroup_configurator_v2() as configurator:
+                tracked = CGroupsTelemetry._tracked
+
+                self.assertTrue(configurator.enabled(), "Cgroups should be enabled")
+                self.assertFalse(
+                    any(cg for cg in tracked if tracked[cg].name == AGENT_NAME_TELEMETRY and 'cpu' in cg),
+                    "The Agent's cpu is being tracked. Tracked: {0}".format(tracked))
+
+    def test_enforcement_not_enabled_in_v2(self):
+        with self._get_cgroup_configurator_v2() as configurator:
+            cmd = 'systemctl set-property walinuxagent.service CPUQuota'
+            self.assertNotIn(cmd, configurator.mocks.commands_call_list, "The command to set CPU quota was called")
 
     def test_initialize_should_not_create_unit_files(self):
         with self._get_cgroup_configurator() as configurator:
@@ -433,32 +466,24 @@ class CGroupConfiguratorSystemdTestCase(AgentTestCase):
                     self.assertIn("A TEST EXCEPTION", str(context_manager.exception))
 
     @patch('time.sleep', side_effect=lambda _: mock_sleep())
-    def test_start_extension_command_should_not_use_systemd_when_cgroup_v2_enabled(self, _):
+    def test_start_extension_command_should_use_systemd_when_cgroup_v2_enabled(self, _):
         with self._get_cgroup_configurator_v2() as configurator:
-            self.assertFalse(configurator.enabled())
+            with patch("azurelinuxagent.ga.cgroupapi.subprocess.Popen", wraps=subprocess.Popen) as popen_patch:
+                configurator.start_extension_command(
+                    extension_name="Microsoft.Compute.TestExtension-1.2.3",
+                    command="the-test-extension-command",
+                    cmd_name="test",
+                    timeout=300,
+                    shell=False,
+                    cwd=self.tmp_dir,
+                    env={}.update(os.environ),
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE)
 
-            with patch("azurelinuxagent.ga.cgroupapi.SystemdCgroupApiv2.start_extension_command") as v2_extension_start_command:
-                with patch("azurelinuxagent.ga.cgroupapi.subprocess.Popen", wraps=subprocess.Popen) as patcher:
-                    configurator.start_extension_command(
-                        extension_name="Microsoft.Compute.TestExtension-1.2.3",
-                        command="date",
-                        cmd_name="test",
-                        timeout=300,
-                        shell=False,
-                        cwd=self.tmp_dir,
-                        env={}.update(os.environ),
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE)
+                command_calls = [args[0] for (args, _) in popen_patch.call_args_list if "the-test-extension-command" in args[0]]
 
-                    command_calls = [args[0] for args, _ in patcher.call_args_list if
-                                     len(args) > 0 and "date" in args[0]]
-                    self.assertFalse(v2_extension_start_command.called)
-                    self.assertEqual(len(command_calls), 1,
-                                     "The test command should have been called exactly once [{0}]".format(
-                                         command_calls))
-                    self.assertNotIn("systemd-run", command_calls[0],
-                                     "The command should not have been invoked using systemd")
-                    self.assertEqual(command_calls[0], "date", "The command line should not have been modified")
+                self.assertEqual(len(command_calls), 1, "The test command should have been called exactly once [{0}]".format(command_calls))
+                self.assertIn("systemd-run", command_calls[0], "The extension should have been invoked using systemd")
 
     @patch('time.sleep', side_effect=lambda _: mock_sleep())
     def test_start_extension_command_should_disable_cgroups_and_invoke_the_command_directly_if_systemd_fails(self, _):
@@ -730,7 +755,7 @@ class CGroupConfiguratorSystemdTestCase(AgentTestCase):
         with self._get_cgroup_configurator() as configurator:
             with patch("os.path.exists") as mock_path:
                 mock_path.side_effect = side_effect
-                CGroupsTelemetry._tracked['/sys/fs/cgroup/cpu,cpuacct/system.slice/extension.service'] = \
+                CGroupsTelemetry._tracked['cpu:/sys/fs/cgroup/cpu,cpuacct/system.slice/extension.service'] = \
                     CpuControllerV1('extension.service', '/sys/fs/cgroup/cpu,cpuacct/system.slice/extension.service')
                 configurator.stop_tracking_unit_cgroups("extension.service")
 
