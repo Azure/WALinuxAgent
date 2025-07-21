23aeec54a89c3e9c47cbe071ea1f6fe09bbbd7c9
Nageswara Nandigam <nnandigam@microsoft.com>
2025-07-21 16:09:51 -0700

    pylint

--- tests_e2e/tests/ext_update/ext_update_failure.py
+++ tests_e2e/tests/ext_update/ext_update_failure.py
@@ -24,7 +24,6 @@ from assertpy import fail, assert_that
 
 from tests_e2e.tests.lib.agent_test import AgentVmTest
 from tests_e2e.tests.lib.logging import log
-from tests_e2e.tests.lib.ssh_client import SshClient
 from tests_e2e.tests.lib.virtual_machine_extension_client import VirtualMachineExtensionClient
 from tests_e2e.tests.lib.vm_extension_identifier import VmExtensionIds, VmExtensionIdentifier
 
