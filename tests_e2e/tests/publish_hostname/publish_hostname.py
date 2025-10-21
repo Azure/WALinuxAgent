dae345a77a74c69c2c0ea22b59f1518fd4f46761
Nageswara Nandigam (HE/HIM) <nnandigam@microsoft.com>
2025-10-21 15:00:21 -0700

    .

--- tests_e2e/tests/publish_hostname/publish_hostname.py
+++ tests_e2e/tests/publish_hostname/publish_hostname.py
@@ -31,13 +31,13 @@ from typing import List, Dict, Any
 from assertpy import fail
 from time import sleep
 
+from tests_e2e.tests.lib.shell import CommandError
 from tests_e2e.tests.lib.test_result import TestSkipped
 from tests_e2e.tests.lib.agent_test import AgentVmTest
 from tests_e2e.tests.lib.agent_test_context import AgentVmTestContext
 from tests_e2e.tests.lib.logging import log
 
 from azurelinuxagent.common.future import UTC
-from tests_e2e.tests.lib.shell import CommandError
 
 
 class PublishHostname(AgentVmTest):
