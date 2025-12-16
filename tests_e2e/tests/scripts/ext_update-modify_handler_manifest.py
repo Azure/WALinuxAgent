b5bb3b6df3551efd323f850fca7519045741638d
Nageswara Nandigam (HE/HIM) <nnandigam@microsoft.com>
2025-12-16 12:15:47 -0800

    add logging

--- tests_e2e/tests/scripts/ext_update-modify_handler_manifest.py
+++ tests_e2e/tests/scripts/ext_update-modify_handler_manifest.py
@@ -50,70 +50,72 @@ def main():
         log.info("No handlerManifest.json file found for extension '{0}'".format(extension_name))
         sys.exit(1)
 
-    manifest_file = matched_files[0]
-    log.info("HandlerManifest file found for extension '{0}': {1}".format(extension_name, manifest_file))
+    log.info("HandlerManifest file found for extension '{0}': {1}".format(extension_name, matched_files))
 
     if reset:
-        log.info("Resetting handlerManifest file for extension '{0}' to default values".format(extension_name))
-        # Reset the handlerManifest file to the default values
-        backup_file = manifest_file + ".bak"
-        if os.path.exists(backup_file):
-            shutil.copy(backup_file, manifest_file)
-            log.info("Reset handlerManifest file for extension '{0}' to default values".format(extension_name))
-        else:
-            log.info("No backup file found to reset the handlerManifest file for extension '{0}'".format(extension_name))
+        for manifest_file in matched_files:
+            log.info("Resetting handlerManifest file for extension '{0}' to default values".format(extension_name))
+            # Reset the handlerManifest file to the default values
+            backup_file = manifest_file + ".bak"
+            if os.path.exists(backup_file):
+                shutil.copy(backup_file, manifest_file)
+                log.info("Reset handlerManifest file for extension '{0}' to default values".format(extension_name))
+            else:
+                log.info("No backup file found to reset the handlerManifest file for extension '{0}'".format(extension_name))
         sys.exit(0)
 
     if len(properties) == 0:
         log.info("No properties provided to update in the handlerManifest file for extension '{0}'".format(extension_name))
         sys.exit(1)
 
-    shutil.copy(manifest_file, manifest_file + ".bak")
+    # Update the handlerManifest file
+    for manifest_file in matched_files:
+        shutil.copy(manifest_file, manifest_file + ".bak")
 
-    # Sample handlerManifest.json structure:
-    # [
-    #   {
-    #     "version": 1.0,
-    #     "handlerManifest": {
-    #       "installCommand": "bin/custom-script-shim install",
-    #       "uninstallCommand": "bin/custom-script-shim uninstall",
-    #       "updateCommand": "bin/custom-script-shim update",
-    #       "enableCommand": "bin/custom-script-shim enable",
-    #       "disableCommand": "bin/custom-script-shim disable",
-    #       "rebootAfterInstall": false,
-    #       "reportHeartbeat": false,
-    #       "updateMode": "UpdateWithInstall"
-    #     },
-    #     "signingInfo": {
-    #       "type": "CustomScript",
-    #       "publisher": "Microsoft.Azure.Extensions",
-    #       "version": "2.1.13"
-    #     }
-    #   }
-    # ]
+        # Sample handlerManifest.json structure:
+        # [
+        #   {
+        #     "version": 1.0,
+        #     "handlerManifest": {
+        #       "installCommand": "bin/custom-script-shim install",
+        #       "uninstallCommand": "bin/custom-script-shim uninstall",
+        #       "updateCommand": "bin/custom-script-shim update",
+        #       "enableCommand": "bin/custom-script-shim enable",
+        #       "disableCommand": "bin/custom-script-shim disable",
+        #       "rebootAfterInstall": false,
+        #       "reportHeartbeat": false,
+        #       "updateMode": "UpdateWithInstall"
+        #     },
+        #     "signingInfo": {
+        #       "type": "CustomScript",
+        #       "publisher": "Microsoft.Azure.Extensions",
+        #       "version": "2.1.13"
+        #     }
+        #   }
+        # ]
 
-    with open(manifest_file, 'r') as file:
-        data = json.load(file)
+        with open(manifest_file, 'r') as file:
+            data = json.load(file)
 
-    commands = data[0]['handlerManifest']
+        commands = data[0]['handlerManifest']
 
-    for prop in properties:
-        # Split the property into cmd_name and cmd_value
-        if '=' not in prop:
-            log.info("Property '{0}' is not in the format 'cmd_name=cmd_value'".format(prop))
-            sys.exit(1)
+        for prop in properties:
+            # Split the property into cmd_name and cmd_value
+            if '=' not in prop:
+                log.info("Property '{0}' is not in the format 'cmd_name=cmd_value'".format(prop))
+                sys.exit(1)
 
-        cmd_name, cmd_value = prop.split('=', 1)
-        log.info("Updating command '{0}' with value '{1}'".format(cmd_name, cmd_value))
+            cmd_name, cmd_value = prop.split('=', 1)
+            log.info("Updating command '{0}' with value '{1}'".format(cmd_name, cmd_value))
 
-        # Update the handlerManifest file
-        log.info("Updating handlerManifest file for extension '{0}' for cmd '{1}' and value '{2}'".format(extension_name, cmd_name, cmd_value))
-        commands[cmd_name] = cmd_value
+            # Update the handlerManifest file
+            log.info("Updating handlerManifest file for extension '{0}' for cmd '{1}' and value '{2}'".format(extension_name, cmd_name, cmd_value))
+            commands[cmd_name] = cmd_value
 
-    with open(manifest_file, 'w') as file:
-        json.dump(data, file, indent=4)
+        with open(manifest_file, 'w') as file:
+            json.dump(data, file, indent=4)
 
-    log.info("Updated the handlerManifest file for extension '{0}'".format(extension_name))
+        log.info("Updated the handlerManifest file for extension '{0}'".format(extension_name))
     sys.exit(0)
 
 
