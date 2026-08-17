5f82968bb3b0ebae8d8b41cda06bb0a29f018132
Nageswara Nandigam (HE/HIM) <nnandigam@microsoft.com>
2026-08-17 12:28:09 -0700

    Add delay for instance view retry

--- tests_e2e/tests/lib/virtual_machine_extension_client.py
+++ tests_e2e/tests/lib/virtual_machine_extension_client.py
@@ -19,6 +19,7 @@
 # This module includes facilities to execute VM extension operations (enable, remove, etc).
 #
 import json
+import time
 import uuid
 
 from assertpy import assert_that, soft_assertions
@@ -143,7 +144,8 @@ class VirtualMachineExtensionClient(AzureSdkClient):
         attempt = 1
         instance_view = self.get_instance_view()
         while instance_view.name is not None and instance_view.type_handler_version is None and instance_view.statuses is None and attempt < 3:
-            log.info("Instance view is incomplete: %s\nRetrying attempt to get instance view...", instance_view.serialize())
+            log.info("Instance view is incomplete: %s\nRetrying attempt after 30 secs delay to get instance view...", instance_view.serialize())
+            time.sleep(30)
             instance_view = self.get_instance_view()
             attempt += 1
         log.info("Instance view:\n%s", json.dumps(instance_view.serialize(), indent=4))
